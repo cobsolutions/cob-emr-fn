1ccb7c05d97436c572cbe7b6c69606461bfc39d1
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-05-27 17:17:50 +0300

    add roles and scopes

---
+++ projects/emr-application/src/app/modules/security/model/role.ts
@@ -0,0 +1,14 @@
+export class Role {
+    public static readonly PATIENT_ROLE = 'patient-role';
+    public static readonly USER_ROLE = 'user-role';
+    public static readonly CLINIC_ROLE = 'clinic-role';
+    public static readonly REFERRING_DOCTOR_ROLE = 'referring-provider-role';
+    public static readonly PATIENT_PAYMENT_ROLE = 'patient-payment-role';
+    public static readonly INSURANCE_COMPANY_ROLE = 'insurance-company-role';
+    public static readonly CALENDAR_ROLE = 'calendar-role';
+    
+    public static readonly MEDICAL_NOTE_ROLE = 'medical-note-role';
+    public static readonly INITIALIZE_MEDICAL_NOTE_ROLE = 'initialize-medical-note-role';
+    public static readonly FORWARD_MEDICAL_NOTE_ROLE = 'forward-medical-note-role';
+    public static readonly FINALIZE_MEDICAL_NOTE_ROLE = 'finalize-medical-note-role';
+}
