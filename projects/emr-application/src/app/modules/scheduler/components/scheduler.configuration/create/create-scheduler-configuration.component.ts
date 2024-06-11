f54d326894e4e8b234ac53614aac50d4316cfad0
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-06-11 10:40:35 +0300

    create skeleton of create scheduler configuration component

---
+++ projects/emr-application/src/app/modules/scheduler/components/scheduler.configuration/create/create-scheduler-configuration.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-scheduler-configuration',
+  templateUrl: './create-scheduler-configuration.component.html',
+  styleUrls: ['./create-scheduler-configuration.component.css']
+})
+export class CreateSchedulerConfigurationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
