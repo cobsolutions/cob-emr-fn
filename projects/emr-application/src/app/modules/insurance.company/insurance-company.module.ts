91d09a718d62cb618dadb51a21e04a4f4cf8e64d
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-06-23 20:44:55 +0300

    create insurance company module

---
+++ projects/emr-application/src/app/modules/insurance.company/insurance-company.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { InsuranceCompanyRoutingModule } from './insurance-company-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    InsuranceCompanyRoutingModule
+  ]
+})
+export class InsuranceCompanyModule { }
