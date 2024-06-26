af2da89ff33f5a5f26695589759fbc0bc5ff1e62
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-06-26 12:57:25 +0300

    add clinic fields and table

--- projects/emr-application/src/app/modules/signup/components/clinic.info/clinic-information.component.ts
+++ projects/emr-application/src/app/modules/signup/components/clinic.info/clinic-information.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Clinic } from '../../../patient/models/clinic';
 
 @Component({
   selector: 'clinic-information',
@@ -8,9 +9,18 @@ import { FormGroup } from '@angular/forms';
 })
 export class ClinicInformationComponent implements OnInit {
   @Input() form: FormGroup;
+  createdClinic: Clinic = {
+    name: '',
+    address: {
+      addressType: null,
+      country: null
+    }
+  };
   constructor() { }
 
   ngOnInit(): void {
   }
+  add() {
 
+  }
 }
