00a73fd724024ede2663dd83ff7fc860e955c0ad
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-06-27 10:35:49 +0300

    show organization summary

--- projects/emr-application/src/app/modules/signup/components/summary/organization-summary.component.ts
+++ projects/emr-application/src/app/modules/signup/components/summary/organization-summary.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
+import { Clinic } from '../../../patient/models/clinic';
+import { Organization } from '../../model/organization';
 
 @Component({
   selector: 'organization-summary',
@@ -8,15 +10,50 @@ import { FormGroup } from '@angular/forms';
 })
 export class OrganizationSummaryComponent implements OnInit {
   @Input() form: FormGroup;
-  
+  organization: Organization = {}
+
   constructor() { }
 
   ngOnInit(): void {
-    this.form.get('Users').valueChanges.forEach(selected=>{
-      console.log(JSON.stringify(selected))
+    this.fillEsstenialInfo();
+    this.fillClinics();
+    this.fillAdministratorUser();
+    this.fillUsers();
+  }
+  submit() {
+    console.log(JSON.stringify(this.form.value))
+  }
+  private fillEsstenialInfo() {
+    this.form.get('Essential').valueChanges.forEach(selected => {
+      this.organization.orgname = selected?.orgname
+      this.organization.dba = selected?.dba
+      this.organization.groupNPI = selected?.groupNPI
+      this.organization.taxID = selected?.taxID
+      this.organization.firstAddress = selected?.firstAddress
+      this.organization.secondAddress = selected?.secondAddress
+      this.organization.state = selected?.state
+      this.organization.city = selected?.city
+      this.organization.zipCode = selected?.zipCode
     })
   }
-  submit(){
-    console.log(JSON.stringify(this.form.value))
+  private fillClinics() {
+    this.form.get('Clinics').valueChanges.forEach(selected => {
+      this.organization.clinics = selected;
+    })
+  }
+  private fillAdministratorUser() {
+    this.form.get('Administrator').valueChanges.forEach(selected => {
+      this.organization.administrator = selected;
+    })
+  }
+  private fillUsers() {
+    this.form.get('Users').valueChanges.forEach(selected => {
+      this.organization.users = selected;
+    })
+  }
+  get organizationAddress() {
+    var address = this.organization.firstAddress + ',' +
+      (this.organization.secondAddress ? this.organization.firstAddress : '') + ',' + this.organization.state + ',' + this.organization.city + ',' + this.organization.zipCode
+    return address;
   }
 }
