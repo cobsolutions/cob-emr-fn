123414decf3e38c78e9f6515d6a19b4d81c89cf6
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-06-28 15:15:34 +0300

    handle administrator user loggedin

--- projects/emr-application/src/app/modules/security/service/kc-auth.guard.ts
+++ projects/emr-application/src/app/modules/security/service/kc-auth.guard.ts
@@ -29,7 +29,6 @@ export class KcAuthGuard extends KeycloakAuthGuard {
       this.keycloakAngular.logout();
     }
     var type = route.data['type'];
-    console.log(JSON.stringify(this.roles))
     if (type === 'requester' && !this.roles.includes(Role.ORGANIZATION_REQUEST_ROLE)) {
       this.keycloakAngular.logout();
     }
