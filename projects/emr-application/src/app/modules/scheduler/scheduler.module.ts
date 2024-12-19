eb338a24222abeee96717fb1ce8a539fd0f68ab3
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-12-19 22:45:01 +0200

    show calendars panel in view scheduler

--- projects/emr-application/src/app/modules/scheduler/scheduler.module.ts
+++ projects/emr-application/src/app/modules/scheduler/scheduler.module.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
-import { CalloutModule, DatePickerModule, ModalModule, SmartPaginationModule, SmartTableModule, SpinnerModule, TimePickerModule } from '@coreui/angular-pro';
+import { CalloutModule, DatePickerModule, ListGroupModule, ModalModule, SmartPaginationModule, SmartTableModule, SpinnerModule, TimePickerModule } from '@coreui/angular-pro';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { AutocompleteLibModule } from 'angular-ng-autocomplete';
@@ -70,6 +70,7 @@ import { CreateCalendarComponent } from './components/calendar.create/create-cal
         SpinnerModule,
         SmartTableModule,
         SmartPaginationModule,
+        ListGroupModule,
         CalendarModule.forRoot({
             provide: DateAdapter,
             useFactory: adapterFactory,
