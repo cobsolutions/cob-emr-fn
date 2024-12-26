e98eca6599b2fec40a996f470254643969e5348b
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-12-26 16:27:51 +0200

    add validation of scheduler date configurations

--- projects/emr-application/src/app/modules/scheduler/components/scheduler.settings/scheduler.date.settings/scheduler.date.settings.component.ts
+++ projects/emr-application/src/app/modules/scheduler/components/scheduler.settings/scheduler.date.settings/scheduler.date.settings.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AppointmentDuration } from '../../../lookups/appointment.duration';
+import { DayHours } from '../../../lookups/day.hours';
+
+import { StartWeek } from '../../../lookups/start.week';
+import { TimeInterval } from '../../../lookups/time.interval';
 
 @Component({
   selector: 'SchedulerDateSettings',
@@ -6,10 +12,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scheduler.date.settings.component.css']
 })
 export class SchedulerDateSettingsComponent implements OnInit {
-
+  settingsForm: FormGroup
+  startweek: string[] = StartWeek;
+  timeInterval: string[] = TimeInterval;
+  dayHours: string[] = DayHours;
+  appointmentDuration: string[] = AppointmentDuration
+  isValidForm:boolean = false;
   constructor() { }
 
   ngOnInit(): void {
+    this.createSettingsForm()
   }
 
+  private createSettingsForm() {
+    this.settingsForm = new FormGroup({
+      'time-interval': new FormControl(null, [Validators.required]),
+      'start-week': new FormControl(null, [Validators.required]),
+      'start-of-day': new FormControl(null, [Validators.required]),
+      'end-of-day': new FormControl(null, [Validators.required]),
+      'appointment-duration': new FormControl(null, [Validators.required]),
+    })
+  }
+  save(){
+    if (this.settingsForm?.valid) {
+      console.log('valid')
+      this.isValidForm = false;
+    }else{
+      this.isValidForm = true;
+    }
+  }
 }
