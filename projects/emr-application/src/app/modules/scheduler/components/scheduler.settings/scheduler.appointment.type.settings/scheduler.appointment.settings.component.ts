f119946d9b71c9783473924900935ccfa20ee33a
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-12-28 20:05:39 +0200

    impl  component to create appoinmtnet type

--- projects/emr-application/src/app/modules/scheduler/components/scheduler.settings/scheduler.appointment.type.settings/scheduler.appointment.settings.component.ts
+++ projects/emr-application/src/app/modules/scheduler/components/scheduler.settings/scheduler.appointment.type.settings/scheduler.appointment.settings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'SchedulerAppointmentSettings',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./scheduler.appointment.settings.component.css']
 })
 export class SchedulerAppointmentSettingsComponent implements OnInit {
-
+  appointmentTypeForm: FormGroup
+  isValidForm: boolean = false;
+  public color: string = '#2889e9';
   constructor() { }
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  private createForm() {
+    this.appointmentTypeForm = new FormGroup({
+      'appointment-name': new FormControl(null, [Validators.required]),
+      'appointment-color': new FormControl(null, [Validators.required]),
+      'appointment-fonrt-color': new FormControl(null, [Validators.required]),
+    })
+  }
+  public onEventLog(event: string, data: any): void {
+  }
+  save() {
+
+  }
 }
