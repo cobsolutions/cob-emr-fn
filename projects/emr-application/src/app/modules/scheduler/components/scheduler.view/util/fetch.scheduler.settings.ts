d93a6005b26bbb188eaf15f84e379e607a85329e
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-12-30 18:16:46 +0200

    set time interval for scheduler settings and assign it

--- projects/emr-application/src/app/modules/scheduler/components/scheduler.view/util/fetch.scheduler.settings.ts
+++ projects/emr-application/src/app/modules/scheduler/components/scheduler.view/util/fetch.scheduler.settings.ts
@@ -5,12 +5,14 @@ export interface Settings {
     dayOfWeek?: DAYS_OF_WEEK;
     startOfDay?: number
     endOfDay?: number
+    timeInterval?: number
 }
 export class FetchSchedulerSettings {
     static schedulerSetting: Settings = {};
     public static setup(schedulerDateSettings: SchedulerSettings) {
         this.setStartWeek(schedulerDateSettings.startWeek)
         this.setDayHours(schedulerDateSettings.startDay, schedulerDateSettings.endDay);
+        this.setTimeInterval(schedulerDateSettings.timeInterval)
         console.log(JSON.stringify(this.schedulerSetting))
         return this.schedulerSetting;
     }
@@ -41,5 +43,35 @@ export class FetchSchedulerSettings {
         const endDayHour = parseTime(endDay);
         this.schedulerSetting.startOfDay = startDayHour;
         this.schedulerSetting.endOfDay = endDayHour;
+    }
+    private static setTimeInterval(timeInterval: string) {
+        const minutesPattern = /(\d+)\s*minutes?/i;
+        const hoursPattern = /(\d+)\s*Hour/i;
+    
+        let minutesValue: number | undefined;
+        let hourSegments: number;
+    
+        // Check for minutes format
+        if (minutesPattern.test(timeInterval)) {
+            const match = timeInterval.match(minutesPattern);
+            minutesValue = match ? parseInt(match[1], 10) : undefined;
+        }
+    
+        // Check for hour format
+        if (hoursPattern.test(timeInterval)) {
+            const match = timeInterval.match(hoursPattern);
+            const hourValue = match ? parseInt(match[1], 10) : undefined;
+    
+            if (hourValue !== undefined) {
+                minutesValue = hourValue * 60; // Convert hours to minutes
+            }
+        }
+    
+        if (minutesValue !== undefined && minutesValue > 0) {
+            hourSegments = 60 / minutesValue;
+        } else {
+            throw new Error("Invalid time interval format or value.");
+        }
+        this.schedulerSetting.timeInterval = hourSegments
     }
 }
