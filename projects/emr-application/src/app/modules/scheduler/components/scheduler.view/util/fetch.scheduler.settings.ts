a46d97aee4f9d0353e62d8d8320ee2db7f96ddbc
madel <mohammed.adel.abo.foutouh@gmail.com>
2024-12-30 17:00:26 +0200

    set start and end hours to week and day getting from scheduler settings

--- projects/emr-application/src/app/modules/scheduler/components/scheduler.view/util/fetch.scheduler.settings.ts
+++ projects/emr-application/src/app/modules/scheduler/components/scheduler.view/util/fetch.scheduler.settings.ts
@@ -1,12 +1,17 @@
 import { DAYS_OF_WEEK } from "calendar-utils";
+import * as moment from "moment";
 import { SchedulerSettings } from "../../../model/shceduler.date.settings";
 export interface Settings {
     dayOfWeek?: DAYS_OF_WEEK;
+    startOfDay?: number
+    endOfDay?: number
 }
 export class FetchSchedulerSettings {
-    static schedulerSetting: Settings={};
+    static schedulerSetting: Settings = {};
     public static setup(schedulerDateSettings: SchedulerSettings) {
         this.setStartWeek(schedulerDateSettings.startWeek)
+        this.setDayHours(schedulerDateSettings.startDay, schedulerDateSettings.endDay);
+        console.log(JSON.stringify(this.schedulerSetting))
         return this.schedulerSetting;
     }
     private static setStartWeek(startWeek: string) {
@@ -18,5 +23,23 @@ export class FetchSchedulerSettings {
                 this.schedulerSetting.dayOfWeek = DAYS_OF_WEEK.SUNDAY
                 break;
         }
+    }
+    private static setDayHours(startDay?: string, endDay?: string) {
+        const parseTime = (time: string): number => {
+            const [hour, modifier] = time.toLowerCase().split(/(am|pm)/);
+            let [hours, minutes] = hour.split(":").map(Number);
+
+            // If no minutes are specified, default to 0
+            if (isNaN(minutes)) minutes = 0;
+
+            if (modifier === "pm" && hours < 12) hours += 12;
+            if (modifier === "am" && hours === 12) hours = 0;
+
+            return hours;
+        };
+        const startDayHour = parseTime(startDay);
+        const endDayHour = parseTime(endDay);
+        this.schedulerSetting.startOfDay = startDayHour;
+        this.schedulerSetting.endOfDay = endDayHour;
     }
 }
