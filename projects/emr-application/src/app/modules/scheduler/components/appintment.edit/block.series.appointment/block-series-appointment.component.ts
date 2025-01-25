7e4919fc9195706f96ad3adea1befd0f020f440a
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-01-25 14:30:20 +0200

    show clinic and title for blcok series appointment

--- projects/emr-application/src/app/modules/scheduler/components/appintment.edit/block.series.appointment/block-series-appointment.component.ts
+++ projects/emr-application/src/app/modules/scheduler/components/appintment.edit/block.series.appointment/block-series-appointment.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { filter } from 'rxjs';
+import { LoggedInService } from '../../../../security/service/loggedIn/logged-in.service';
 import { AppointmentBlockSeries } from '../../../models/appointment.block.series';
 
 @Component({
@@ -8,9 +11,22 @@ import { AppointmentBlockSeries } from '../../../models/appointment.block.series
 })
 export class BlockSeriesAppointmentComponent implements OnInit {
   @Input() appointmentBlockSeries: AppointmentBlockSeries
-  constructor() { }
+  toBeEdit: AppointmentBlockSeries = {}
+  compareFn = this._compareFn.bind(this);
+  startTime: Date;
+  endTime: Date
+  constructor(private loggedInService: LoggedInService) { }
 
   ngOnInit(): void {
+    this.initModel()
+  }
+  _compareFn(a, b) {
+    return Number(a?.id) === Number(b?.id);
+  }
+
+  private initModel() {
+    this.startTime = moment.unix(this.appointmentBlockSeries.startTime / 1000).toDate();
+    this.endTime = moment.unix(this.appointmentBlockSeries.endTime / 1000).toDate();
   }
 
 }
