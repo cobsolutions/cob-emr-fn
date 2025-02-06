c07be6f32d96940c44d6f9ea22677a2345db4db9
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-02-06 12:50:52 +0200

    add table to patient payment component to render records

--- projects/emr-application/src/app/modules/patient/components/chart/payment/patient-payment.component.ts
+++ projects/emr-application/src/app/modules/patient/components/chart/payment/patient-payment.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { IColumn } from '@coreui/angular-pro/lib/smart-table/smart-table.type';
+import { map, Observable, retry, tap } from 'rxjs';
+import { ListTemplate } from '../../../../common/template/list.template';
+import { PatientPaymentRecordResponse } from '../../../models/chart/patient.payment/patient.payment.record.response';
+import { PatientPaymentService } from '../../../services/patient/payment/patient-payment.service';
 
 @Component({
   selector: 'patient-payment',
   templateUrl: './patient-payment.component.html',
   styleUrls: ['./patient-payment.component.css']
 })
-export class PatientPaymentComponent implements OnInit {
-
-  constructor() { }
+export class PatientPaymentComponent extends ListTemplate implements OnInit {
+  patientPaymentRecordResponse$!: Observable<PatientPaymentRecordResponse[]>;
+  columns: (string | IColumn)[];
+  @Input() patientId: number;
+  constructor(private patientPaymentService: PatientPaymentService) { super(); }
 
   ngOnInit(): void {
+    this.columns = this.constructColumns(['amount', 'reason', 'createdAt', 'Actions']);
+    this.getPatientPayments();
   }
 
+  private getPatientPayments() {
+    this.patientPaymentRecordResponse$ = this.patientPaymentService.findPatientPayments(this.apiParams$, this.patientId).pipe(
+      retry({
+        delay: (error) => {
+          console.warn('Retry: ', error);
+          this.errorMessage$.next(error.message ?? `Error: ${JSON.stringify(error)}`);
+          this.loadingData$.next(false);
+          return this.retry$;
+        }
+      }),
+      tap((response: any) => {
+        this.totalItems$.next(response.number_of_matching_records);
+        if (response.number_of_records) {
+          this.errorMessage$.next('');
+        }
+        this.retry$.next(false);
+        this.loadingData$.next(false);
+      }),
+      map((response: any) => {
+        return response.records;
+      })
+    )
+  }
 }
---
+++ projects/emr-application/src/app/modules/patient/services/patient/payment/patient-payment.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'projects/emr-application/src/environments/environment';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { IApiParams } from '../../../../common/interfaces/api.params';
+import { BasePaginationService } from '../../../../common/service/base-pagination.service';
+import { LoggedInService } from '../../../../security/service/loggedIn/logged-in.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientPaymentService extends BasePaginationService {
+  private baseUrl = environment.baseURL + 'patient/payment'
+
+  constructor(httpClient: HttpClient, loggedInService: LoggedInService) { super(httpClient, loggedInService) }
+
+  public findPatientPayments(config$: BehaviorSubject<IApiParams>,
+    pateintId: number,
+  ): Observable<any> {
+    return this._get(config$, this.baseUrl + '/find/patientId/' + pateintId);
+  }
+
+}
