5563e4ac595d2b31174e60bf404874708be04b0f
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-02-19 10:06:38 +0200

    add all fields inputs in current function component

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/subjective/current.function.limit/current-funvtionlimit.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/subjective/current.function.limit/current-funvtionlimit.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-current-funvtionlimit',
+  selector: 'current-functionlimit',
   templateUrl: './current-funvtionlimit.component.html',
   styleUrls: ['./current-funvtionlimit.component.css']
 })
 export class CurrentFunvtionlimitComponent implements OnInit {
-
-  constructor() { }
+  currentFunctionForm: FormGroup;
+  @Output() formReady = new EventEmitter<FormGroup>();
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.currentFunctionForm = this.fb.group({
+      'self_care': new FormControl(null, [Validators.required]),
+      'mobility': new FormControl(null, [Validators.required]),
+      'other': new FormControl(null, [Validators.required]),
+
+      'body_position': new FormControl(null, [Validators.required]),
+      'handling_object': new FormControl(null, [Validators.required]),
+
+
+    })
+    this.formReady.emit(this.currentFunctionForm);
   }
 
 }
