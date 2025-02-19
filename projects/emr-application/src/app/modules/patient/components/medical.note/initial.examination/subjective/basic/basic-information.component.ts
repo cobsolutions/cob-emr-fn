f60649f5e3e67a6d45a18c0a909bd15239d7fad4
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-02-19 09:30:08 +0200

    add all input fields in basic component

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/subjective/basic/basic-information.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/subjective/basic/basic-information.component.ts
@@ -19,10 +19,24 @@ export class BasicInformationComponent implements OnInit {
       'time_in_out': new FormControl(null, [Validators.required]),
       'number_of_visit': new FormControl(null, [Validators.required]),
       'diagnosis': new FormArray([]),
-      'treatment_diagnosis': new FormArray([])
+      'treatment_diagnosis': new FormArray([]),
+      'treatment_side': new FormControl(null, [Validators.required]),
+      'physician_order': new FormControl(null, [Validators.required]),
+      'change_status_date': new FormControl(null, [Validators.required]),
+
+      'chronic': new FormControl(null, [Validators.required]),
+      'insidious': new FormControl(null, [Validators.required]),
+      'new_injury': new FormControl(null, [Validators.required]),
+      'no_new_injury': new FormControl(null, [Validators.required]),
+
+      'surgery_performed': new FormControl(null, [Validators.required]),
+      'prior_hospitalization': new FormControl(null, [Validators.required]),
+
+      'mechanism_injury': new FormControl(null, [Validators.required]),
+      'chief_complaint': new FormControl(null, [Validators.required]),
 
     });
-    this.formReady.emit(this.basicForm);
+  
   }
 
 }
