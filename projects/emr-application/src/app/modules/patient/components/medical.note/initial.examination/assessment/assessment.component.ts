d742c4aac02c58d63462bc61fec5bd1c6358a32b
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-02-24 22:21:42 +0200

    refer assessment in init component

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/assessment/assessment.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/assessment/assessment.component.ts
@@ -1,12 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
 
 @Component({
-  selector: 'app-assessment',
+  selector: 'assessment',
   templateUrl: './assessment.component.html',
   styleUrls: ['./assessment.component.css']
 })
 export class AssessmentComponent implements OnInit {
-
+  objectiveForm: FormGroup;
+  @Output() formReady = new EventEmitter<FormGroup>();
+  @Input() stepper!: MatStepper
   constructor() { }
 
   ngOnInit(): void {
