cf9dc221bb8a658935ce1c25ab92d4ab67fd4131
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-02-24 23:05:14 +0200

    add fields assessment

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/assessment/assessment.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/assessment/assessment.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { MatStepper } from '@angular/material/stepper';
 
 @Component({
@@ -8,12 +8,25 @@ import { MatStepper } from '@angular/material/stepper';
   styleUrls: ['./assessment.component.css']
 })
 export class AssessmentComponent implements OnInit {
-  objectiveForm: FormGroup;
+  assessmentForm: FormGroup;
   @Output() formReady = new EventEmitter<FormGroup>();
   @Input() stepper!: MatStepper
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.buildForm()
+  }
+  private buildForm() {
+    this.assessmentForm = this.fb.group({
+      'assessment_diagnosis': new FormControl(null),
+      'patient_clinical_presentation': new FormControl(null),
+      'parent_patient_education': new FormControl(null),
+      'hep': new FormControl(null),
+      'rehab_potential': new FormControl("excellent"),
+      'contraindications_to_therapy': new FormControl(null),
+      'patientAgreement': new FormControl(null),
+      'consent_to_care': new FormControl(null)
+    })
   }
 
 }
