8e3c419c0c3fb21b3c11d3182867ab58ccfb3d1f
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-02-25 10:12:30 +0200

    create goal in assessment component

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/assessment/assessment.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/assessment/assessment.component.ts
@@ -12,10 +12,15 @@ export class AssessmentComponent implements OnInit {
   @Output() formReady = new EventEmitter<FormGroup>();
   @Input() stepper!: MatStepper
   problemsArray: FormArray;
+  goalsArray: FormArray;
 
   get problems(): FormArray {
     return this.assessmentForm.get('problems') as FormArray;
   }
+  get goals(): FormArray {
+    return this.assessmentForm.get('goals') as FormArray;
+  }
+
   addProblem(input: HTMLInputElement) {
     const problemValue = input.value.trim();
     if (problemValue) {
@@ -26,6 +31,32 @@ export class AssessmentComponent implements OnInit {
 
   removeProblem(index: number) {
     this.problems.removeAt(index); // Remove value from FormArray
+  }
+  createGoal(description = '', term = 'Short Term', period = '1 Visit', met = 'N/A'): FormGroup {
+    return this.fb.group({
+      description: new FormControl(description),  
+      term: new FormControl(term), 
+      period: new FormControl(period),  
+      met: new FormControl(met)        
+    });
+  }
+  addGoal(descriptionInput: HTMLTextAreaElement, termInput: HTMLSelectElement, periodInput: HTMLSelectElement, metInput: HTMLSelectElement) {
+    const description = descriptionInput.value.trim();
+    const term = termInput.value;
+    const period = periodInput.value;
+    const met = metInput.value;
+
+    if (description) {
+      this.goals.push(this.createGoal(description, term, period, met));
+      descriptionInput.value = ''; // Clear description field
+    }
+  }
+
+  removeGoal(index: number) {
+    this.goals.removeAt(index);
+  }
+  getGoalFormGroup(index: number): FormGroup {
+    return this.goals.at(index) as FormGroup; // Ensure each item is treated as FormGroup
   }
   constructor(private fb: FormBuilder) { }
 
@@ -42,9 +73,11 @@ export class AssessmentComponent implements OnInit {
       'contraindications_to_therapy': new FormControl(null),
       'patientAgreement': new FormControl(null),
       'consent_to_care': new FormControl(null),
-      problems: this.fb.array([])
+      problems: this.fb.array([]),
+      goals: this.fb.array([])
     })
     this.problemsArray = this.assessmentForm.get('problems') as FormArray;
+    this.goalsArray = this.assessmentForm.get('goals') as FormArray;
 
   }
 
