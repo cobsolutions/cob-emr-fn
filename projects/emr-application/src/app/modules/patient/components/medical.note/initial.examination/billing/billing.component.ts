514f8387c2cc6e7de7149edacb0e8f05e5a3430f
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-02-25 19:59:08 +0200

    refer billing in init component

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/billing/billing.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/billing/billing.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
 
 @Component({
-  selector: 'app-billing',
+  selector: 'billing',
   templateUrl: './billing.component.html',
   styleUrls: ['./billing.component.css']
 })
 export class BillingComponent implements OnInit {
-
-  constructor() { }
+  billingForm: FormGroup;
+  @Output() formReady = new EventEmitter<FormGroup>();
+  @Input() stepper!: MatStepper
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.billingForm = this.fb.group({});
   }
-
+  next() {
+    this.stepper.next();
+  }
 }
