a3c95a92dfc3d3157f94cebbddb7f2b98a1142c2
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-06 23:57:29 +0200

    design Previous History of Similar Symptoms of medical history section of subjective

---
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/subjective/medical.history/styles/medical.history.ts
@@ -0,0 +1,6 @@
+import { FieldControlStyles } from "../../../../filed.control.style.selector/field.control.style";
+import { PreviousHistoryOfSimilarSymptomsStyles } from "./previous.history.of.similar.symptoms";
+
+export var MedicalHistoryStyles: FieldControlStyles[] = [
+    ...PreviousHistoryOfSimilarSymptomsStyles
+]
---
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/subjective/medical.history/styles/previous.history.of.similar.symptoms.ts
@@ -0,0 +1,33 @@
+import { FieldControlStyles } from "../../../../filed.control.style.selector/field.control.style";
+
+export var PreviousHistoryOfSimilarSymptomsStyles: FieldControlStyles[] = [
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "251_previousHistoryOfSimilarSymptoms"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "252_previousEpisodesOfSameComplaints",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "255_previousTreatmentsForSimilarSymptoms",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "253_ageRange",
+        "style": "margin-left: 20px;width:300px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "254_yearOfFirstEpisode",
+        "style": "margin-left: 20px;width:300px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "256_comment",
+        "style": "margin-left: 20px;width:300px"
+    }
+]
