74fc17c1f174752ee032c4564b8bdf8560624d0d
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-07 00:08:35 +0200

    design General Health of medical history

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/subjective/medical.history/styles/medical.history.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/subjective/medical.history/styles/medical.history.ts
@@ -1,6 +1,8 @@
 import { FieldControlStyles } from "../../../../filed.control.style.selector/field.control.style";
+import { GeneralHealthStyles } from "./general.health";
 import { PreviousHistoryOfSimilarSymptomsStyles } from "./previous.history.of.similar.symptoms";
 
 export var MedicalHistoryStyles: FieldControlStyles[] = [
-    ...PreviousHistoryOfSimilarSymptomsStyles
+    ...PreviousHistoryOfSimilarSymptomsStyles,
+    ...GeneralHealthStyles
 ]
