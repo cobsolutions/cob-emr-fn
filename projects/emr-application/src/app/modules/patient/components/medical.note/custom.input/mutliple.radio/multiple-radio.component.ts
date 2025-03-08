f2014149ceae7c75eced321c8a696867498e983a
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-08 20:21:06 +0200

    impl custom multiple radio

--- projects/emr-application/src/app/modules/patient/components/medical.note/custom.input/mutliple.radio/multiple-radio.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/custom.input/mutliple.radio/multiple-radio.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-multiple-radio',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./multiple-radio.component.css']
 })
 export class MultipleRadioComponent implements OnInit {
-
-  constructor() { }
+  @Input() form: FormGroup;
+  @Input() values:any
+  @Input() label:string
+  @Input() splitColumn:number
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    // Initialize checkboxes with FormControls
+    this.values.forEach(value => {
+      this.form.addControl(value.val, this.fb.control(false));
+      if (value.dependencies !== undefined)
+        value.dependencies.forEach(dep => {
+          this.form.addControl(dep.fieldFormName, this.fb.control(''));
+        })
+    });
+  }
+  toggleAdditionalControl(conditionKey: string) {
+    const isChecked = this.form.get(conditionKey)?.value;
+    if (!isChecked) {
+      this.form.get(conditionKey)?.reset();
+    }
   }
 
 }
