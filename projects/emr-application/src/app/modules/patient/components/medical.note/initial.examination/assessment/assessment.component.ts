b159ab1ac6ca921c63be72798cab66db1d4f6ef6
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-09 03:36:00 +0200

    revamp assessment bild form

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/assessment/assessment.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/assessment/assessment.component.ts
@@ -78,14 +78,6 @@ export class AssessmentComponent implements OnInit {
 
   private buildForm() {
     this.assessmentForm = this.fb.group({
-      'assessment_diagnosis': new FormControl(null),
-      'patient_clinical_presentation': new FormControl(null),
-      'parent_patient_education': new FormControl(null),
-      'hep': new FormControl(null),
-      'rehab_potential': new FormControl("excellent"),
-      'contraindications_to_therapy': new FormControl(null),
-      'patientAgreement': new FormControl(null),
-      'consent_to_care': new FormControl(null),
       problems: this.fb.array([]),
       goals: this.fb.array([])
     })
