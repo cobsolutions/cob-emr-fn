c4aa76fd2b8eed236ba23f51f091c649b5704ceb
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-10 23:13:54 +0200

    add Diathermy

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/plan/components/modalities/styles/diathermy.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/plan/components/modalities/styles/diathermy.ts
@@ -1,3 +1,13 @@
 import { FieldControlStyles } from "../../../../../filed.control.style.selector/field.control.style";
 
-export const DiathermyStyles: FieldControlStyles[] = [];
+export const DiathermyStyles: FieldControlStyles[] =[
+    {
+        "label_style": "white-space: nowrap;",
+        "name": "5_diathermy",
+    },
+    {
+        "label_style": "white-space: nowrap;margin-left: 10px;",
+        "name": "6_duration",
+        "style": "margin-left: 10px;"
+    }
+]
