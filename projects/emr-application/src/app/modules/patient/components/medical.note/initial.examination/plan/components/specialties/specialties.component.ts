15ead92230e135a50942ca2f75ca5110451752b0
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-10 23:49:34 +0200

    add Specialties items

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/plan/components/specialties/specialties.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/plan/components/specialties/specialties.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FieldDependentsService } from 'projects/emr-application/src/app/modules/patient/services/medical.note/field.dependents.builder/field-dependents.service';
 import { FieldControlStyles } from '../../../../filed.control.style.selector/field.control.style';
 import { SpecialtiesStyles } from './styles/specialties';
 
@@ -13,9 +14,12 @@ export class SpecialtiesComponent implements OnInit {
   @Output() formReady = new EventEmitter<FormGroup>();
   @Input() fields: any
   styles: FieldControlStyles[] = SpecialtiesStyles;
-  constructor() { }
+  constructor(private fb: FormBuilder, private fieldDependentsService:FieldDependentsService) { }
 
   ngOnInit(): void {
+    this.fields = this.fieldDependentsService.buildHierarchyRecursive(this.fields);
+    this.specialtiesForm = this.fb.group({})
+    this.formReady.emit(this.specialtiesForm)
   }
   getstyleFieldControl(fieldName: string): FieldControlStyles {
     return this.styles.find(obj => obj.name === fieldName);
--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/plan/components/specialties/styles/specialties.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/plan/components/specialties/styles/specialties.ts
@@ -1,5 +1,41 @@
 import { FieldControlStyles } from "../../../../../filed.control.style.selector/field.control.style";
 
 export const SpecialtiesStyles: FieldControlStyles[] = [
+    {
+        "label_style": "white-space: nowrap; ",
+        "name": "30_orthoticFabrication",
+    },
+    {
+        "label_style": "white-space: nowrap;",
+        "name": "31_orthoticFabricationText",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap;",
+        "name": "32_tENSFitting",
+    },
+    {
+        "label_style": "white-space: nowrap;",
+        "name": "33_orthoticFabricationText",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap;",
+        "name": "36_acupuncture",
+    },
+    {
+        "label_style": "white-space: nowrap; ",
+        "name": "37_acupunctureText",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap;",
+        "name": "38_other",
+    },
+    {
+        "label_style": "white-space: nowrap;",
+        "name": "39_otherText",
+        "style": "margin-left: 10px;"
+    }
 
 ]
