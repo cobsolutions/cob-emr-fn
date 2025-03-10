7925b6e6853fefbf52d69b03ee374b87e153db3e
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-10 23:06:34 +0200

    add laser sub item

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/plan/components/modalities/styles/laser.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/plan/components/modalities/styles/laser.ts
@@ -1,3 +1,18 @@
 import { FieldControlStyles } from "../../../../../filed.control.style.selector/field.control.style";
 
-export const LaserStyles: FieldControlStyles[] = [];
+export const LaserStyles: FieldControlStyles[] =[
+    {
+        "label_style": "white-space: nowrap; ",
+        "name": "0_laser",
+    },
+    {
+        "label_style": "white-space: nowrap; ",
+        "name": "1_coldLaser",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; ",
+        "name": "2_class4Laser",
+        "style": "margin-left: 10px;"
+    }
+]
