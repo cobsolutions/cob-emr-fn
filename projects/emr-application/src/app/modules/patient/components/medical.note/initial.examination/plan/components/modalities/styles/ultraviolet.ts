80e6497b69951eb85f7784457b444b388d3ee42a
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-10 23:17:11 +0200

    add Vasopneumatic

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/plan/components/modalities/styles/ultraviolet.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/plan/components/modalities/styles/ultraviolet.ts
@@ -1,3 +1,13 @@
 import { FieldControlStyles } from "../../../../../filed.control.style.selector/field.control.style";
 
-export const UltravioletStyles: FieldControlStyles[] = [];
+export const UltravioletStyles: FieldControlStyles[] = [
+    {
+        "label_style": "white-space: nowrap;",
+        "name": "7_ultraviolet",
+    },
+    {
+        "label_style": "white-space: nowrap; margin-left: 10px;",
+        "name": "8_duration",
+        "style": "margin-left: 10px;"
+    }
+]
