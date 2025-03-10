a8c51795c8f60df8aacc243eee5cb57a0269b1ad
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-10 23:01:05 +0200

    add  Ultrasound/Phonophoresis

--- projects/emr-application/src/app/modules/patient/components/medical.note/custom.input/multiple.checkbox/mutliple-check-box.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/custom.input/multiple.checkbox/mutliple-check-box.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
-import { MatLabel } from '@angular/material/form-field';
 
 @Component({
   selector: 'mutliple-check-box',
@@ -9,12 +8,14 @@ import { MatLabel } from '@angular/material/form-field';
 })
 export class MutlipleCheckBoxComponent implements OnInit {
   @Input() form: FormGroup;
-  @Input() values:any
-  @Input() label:string
-  @Input() splitColumn:number
+  @Input() values: any
+  @Input() label: string
+  @Input() splitColumn: number
+  @Input() labelStyle: string;
+  @Input() checkBoxStyle: string;
   constructor(private fb: FormBuilder) {
   }
-  
+
 
   ngOnInit() {
     // Initialize checkboxes with FormControls
@@ -25,6 +26,11 @@ export class MutlipleCheckBoxComponent implements OnInit {
           this.form.addControl(dep.fieldFormName, this.fb.control(''));
         })
     });
+    if (this.labelStyle === null || this.labelStyle === undefined) {
+      this.labelStyle = "display: flex;align-items: center;gap: 10px;margin-bottom: 1px;margin-left: 400px;max-width:500px;"
+    }
+    if(this.checkBoxStyle == null || this.checkBoxStyle === undefined)
+    this.checkBoxStyle = 'margin-left: 510px;'
   }
   toggleAdditionalControl(conditionKey: string) {
     const isChecked = this.form.get(conditionKey)?.value;
