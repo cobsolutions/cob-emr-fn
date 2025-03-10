72b13b3f99609c35b515b555132cd505f1d92d4c
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-10 23:35:59 +0200

    add Cryotherapy Hot Packs Mechanical Traction

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/plan/components/modalities/styles/cryotherapy.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/plan/components/modalities/styles/cryotherapy.ts
@@ -1,3 +1,23 @@
 import { FieldControlStyles } from "../../../../../filed.control.style.selector/field.control.style";
 
-export const CryotherapyStyles: FieldControlStyles[] = [];
+export const CryotherapyStyles: FieldControlStyles[] =[
+    {
+        "label_style": "white-space: nowrap;",
+        "name": "21_cryotherapy",
+    },
+    {
+        "label_style": "white-space: nowrap; margin-left: 10px;",
+        "name": "22_icePack",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; margin-left: 10px;",
+        "name": "23_iceMassage",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; margin-left: 10px;",
+        "name": "24_duration",
+        "style": "margin-left: 10px;"
+    }
+]
