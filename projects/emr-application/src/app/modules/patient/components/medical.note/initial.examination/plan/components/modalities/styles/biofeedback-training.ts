1acca2823aec361de637bc532ee26577c620344a
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-10 23:20:35 +0200

    add Biofeedback Training

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/plan/components/modalities/styles/biofeedback-training.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/plan/components/modalities/styles/biofeedback-training.ts
@@ -1,3 +1,18 @@
 import { FieldControlStyles } from "../../../../../filed.control.style.selector/field.control.style";
 
-export const BiofeedbackTrainingStyles: FieldControlStyles[] = [];
+export const BiofeedbackTrainingStyles: FieldControlStyles[] =
+    [{
+        "label_style": "white-space: nowrap",
+        "name": "11_biofeedbackTraining",
+    },
+    {
+        "label_style": "white-space: nowrap;margin-left: 10px;",
+        "name": "12_biofeedbackTrainingText",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap;margin-left: 10px;",
+        "name": "13_duration",
+        "style": "margin-left: 10px;"
+    }
+    ]
