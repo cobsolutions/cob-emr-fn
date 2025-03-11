9820f09dbbcec43075b9ff3c1be8cd334d37c109
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-11 20:10:21 +0200

    create CalendarMonth component and referr to it in the billing component

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/billing/components/calendar.month/calendar-month.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/billing/components/calendar.month/calendar-month.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FieldDependentsService } from 'projects/emr-application/src/app/modules/patient/services/medical.note/field.dependents.builder/field-dependents.service';
+import { FieldControlStyles } from '../../../../filed.control.style.selector/field.control.style';
+import { CalendarMonthStyles } from './styles/calendar.month';
 
 @Component({
-  selector: 'app-calendar-month',
+  selector: 'calendar-month',
   templateUrl: './calendar-month.component.html',
   styleUrls: ['./calendar-month.component.css']
 })
 export class CalendarMonthComponent implements OnInit {
-
-  constructor() { }
+  CalendarMonthForm: FormGroup;
+  @Output() formReady = new EventEmitter<FormGroup>();
+  @Input() fields: any
+  styles: FieldControlStyles[] = CalendarMonthStyles;
+  constructor(private fb: FormBuilder, private fieldDependentsService: FieldDependentsService) { }
 
   ngOnInit(): void {
+    this.fields = this.fieldDependentsService.buildHierarchyRecursive(this.fields);
+    this.CalendarMonthForm = this.fb.group({})
+    this.formReady.emit(this.CalendarMonthForm);
   }
-
+  getstyleFieldControl(fieldName: string): FieldControlStyles {
+    return this.styles.find(obj => obj.name === fieldName);
+  }
 }
