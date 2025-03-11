c287c5676ed634f90b3ffd4ac3e1250d4e39d49d
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-11 21:10:44 +0200

    add Respiratory sub item of billing section

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/billing/components/respiratory/respiratory.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/billing/components/respiratory/respiratory.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FieldDependentsService } from 'projects/emr-application/src/app/modules/patient/services/medical.note/field.dependents.builder/field-dependents.service';
 
 @Component({
-  selector: 'app-respiratory',
+  selector: 'respiratory',
   templateUrl: './respiratory.component.html',
   styleUrls: ['./respiratory.component.css']
 })
 export class RespiratoryComponent implements OnInit {
-
-  constructor() { }
+  RespiratoryForm: FormGroup;
+  @Output() formReady = new EventEmitter<FormGroup>();
+  @Input() fields: any
+  constructor(private fb: FormBuilder, private fieldDependentsService: FieldDependentsService) { }
 
   ngOnInit(): void {
+    this.fields = this.fieldDependentsService.buildHierarchyRecursive(this.fields);
+    this.RespiratoryForm = this.fb.group({})
+    this.formReady.emit(this.RespiratoryForm);
   }
 
 }
