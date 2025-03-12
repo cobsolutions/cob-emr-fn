b8dac9db87dafa07114747e7b0e1a254258aa8b4
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-12 02:48:43 +0200

    add Splints Orthotics sub item of billing component

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/billing/components/splints.orthotics/splints-orthotics.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/billing/components/splints.orthotics/splints-orthotics.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { FieldDependentsService } from 'projects/emr-application/src/app/modules/patient/services/medical.note/field.dependents.builder/field-dependents.service';
 
 @Component({
-  selector: 'app-splints-orthotics',
+  selector: 'splints-orthotics',
   templateUrl: './splints-orthotics.component.html',
   styleUrls: ['./splints-orthotics.component.css']
 })
 export class SplintsOrthoticsComponent implements OnInit {
-
-  constructor() { }
+  SplintsOrthoticsForm: FormGroup;
+  @Output() formReady = new EventEmitter<FormGroup>();
+  @Input() fields: any
+  constructor(private fb: FormBuilder, private fieldDependentsService: FieldDependentsService) { }
 
   ngOnInit(): void {
+    this.fields = this.fieldDependentsService.buildHierarchyRecursive(this.fields);
+    this.SplintsOrthoticsForm = this.fb.group({})
+    this.formReady.emit(this.SplintsOrthoticsForm);
   }
-
 }
