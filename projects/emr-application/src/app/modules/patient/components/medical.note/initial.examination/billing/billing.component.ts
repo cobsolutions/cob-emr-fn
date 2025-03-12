f63b04bf6f32ac09ae9a15ff5124934d5c1d0c33
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-12 02:41:45 +0200

    add Supplies sub item to billing section

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/billing/billing.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/billing/billing.component.ts
@@ -294,6 +294,7 @@ export class BillingComponent implements OnInit {
         respiratory: this.fb.group({}),
         nerveConduction: this.fb.group({}),
         otherTreatmentProcedures: this.fb.group({}),
+        supplies: this.fb.group({}),
       });
       this.untimedCodes.forEach(obj => this.billingForm.addControl(obj.value, this.fb.control(false)));
       this.untimedCodesText.forEach(obj => this.billingForm.addControl(obj.code, this.fb.control(false)));
--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/billing/components/supplies/supplies.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/billing/components/supplies/supplies.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FieldDependentsService } from 'projects/emr-application/src/app/modules/patient/services/medical.note/field.dependents.builder/field-dependents.service';
 
 @Component({
-  selector: 'app-supplies',
+  selector: 'supplies',
   templateUrl: './supplies.component.html',
   styleUrls: ['./supplies.component.css']
 })
 export class SuppliesComponent implements OnInit {
 
-  constructor() { }
+  SuppliesForm: FormGroup;
+  @Output() formReady = new EventEmitter<FormGroup>();
+  @Input() fields: any
+  constructor(private fb: FormBuilder, private fieldDependentsService: FieldDependentsService) { }
 
   ngOnInit(): void {
+    this.fields = this.fieldDependentsService.buildHierarchyRecursive(this.fields);
+    this.SuppliesForm = this.fb.group({})
+    this.formReady.emit(this.SuppliesForm);
   }
 
 }
