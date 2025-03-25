1bc897b9c772929f57bb00d5fa954d3630488f77
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-25 03:25:10 +0200

    fill Untimed Codes of billing

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/billing/components/untimedCodes/untimed-codes.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/billing/components/untimedCodes/untimed-codes.component.ts
@@ -13,13 +13,18 @@ export class UntimedCodesComponent implements OnInit {
   UntimedCodesForm: FormGroup;
   @Output() formReady = new EventEmitter<FormGroup>();
   @Input() fields: any
-  @Input() strappingfields : any
+  @Input() strappingfields: any
+  @Input() untimedCodesData: any
   styles: FieldControlStyles[] = UntimedCodesStyles;
   constructor(private fb: FormBuilder, private fieldDependentsService: FieldDependentsService) { }
 
   ngOnInit(): void {
     this.fields = this.fieldDependentsService.buildHierarchyRecursive(this.fields);
     this.UntimedCodesForm = this.fb.group({})
+    if (this.untimedCodesData)
+      setTimeout(() => {
+        this.UntimedCodesForm.patchValue(this.untimedCodesData);
+      }, 10);
     this.formReady.emit(this.UntimedCodesForm);
   }
   getstyleFieldControl(fieldName: string): FieldControlStyles {
