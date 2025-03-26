c32eb99af1857e84d5c13b30cfd69f842667da18
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-26 22:50:56 +0200

    finalize Post Operative/Wound Healing

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/inspection/inspection.fields.styles.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/inspection/inspection.fields.styles.ts
@@ -41,15 +41,78 @@ export var InspectionFormStyles: FieldControlStyles[] = [
         "name": "1_girthMeasurementUpperMultipleText",
         "style": "margin-left: 10px;"
     },
+
+
+
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "2_postOperativeWoundHealing",
+        "name": "9_postOperativeWoundHealing",
         "style": "margin-left: 10px;"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "3_incisionSites",
+        "name": "10_incisionSites",
         "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "11_surgicalPrecautions",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "14_scarMobility",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "16_scarTybe",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "18_woundDescription",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "20_woundMeasurements",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "12_surgicalPrecautionsselect",
+        "style": "margin-left: 20px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "13_compliantWithROMRistrictions",
+        "style": "margin-left: 20px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "15_scarMobilityText",
+        "style": "margin-left: 20px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "17_scarTybeSelect",
+        "style": "margin-left: 20px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "19_woundDescriptionText",
+        "style": "margin-left: 20px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "21_length",
+        "style": "margin-left: 20px;max-width:50px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "22_width",
+        "style": "margin-left: 20px;max-width:50px"
     }
 
 ]
