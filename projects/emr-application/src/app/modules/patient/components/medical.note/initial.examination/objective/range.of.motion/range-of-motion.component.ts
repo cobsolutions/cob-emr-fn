7230354ed575603b17af3110e88c1723ded484eb
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-29 03:30:00 +0200

    create range of motion component and render first level of controls

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/range.of.motion/range-of-motion.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/range.of.motion/range-of-motion.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FieldDependentsService } from '../../../../../services/medical.note/field.dependents.builder/field-dependents.service';
+import { FieldControlStyles } from '../../../filed.control.style.selector/field.control.style';
+import { ROMFormStyles } from './rom.fields.styles';
 
 @Component({
-  selector: 'app-range-of-motion',
+  selector: 'range-of-motion',
   templateUrl: './range-of-motion.component.html',
   styleUrls: ['./range-of-motion.component.css']
 })
 export class RangeOfMotionComponent implements OnInit {
+  ROMForm: FormGroup;
+  @Output() formReady = new EventEmitter<FormGroup>();
+  @Input() fields: any
+  styles: FieldControlStyles[] = ROMFormStyles;
+  constructor(private fb: FormBuilder
+    , private fieldDependentsService: FieldDependentsService) { }
 
-  constructor() { }
 
   ngOnInit(): void {
+    this.fields = this.fieldDependentsService.buildHierarchyRecursive(this.fields);
+    this.ROMForm = this.fb.group({});
+    this.formReady.emit(this.ROMForm);
   }
-
+  getstyleFieldControl(fieldName: string): FieldControlStyles {
+    return this.styles.find(obj => obj.name === fieldName);
+  }
 }
---
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/range.of.motion/rom.fields.styles.ts
@@ -0,0 +1,13 @@
+import { FieldControlStyles } from "../../../filed.control.style.selector/field.control.style";
+
+export var ROMFormStyles: FieldControlStyles[] = [
+    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "9_noLimitationsNoted", "style": "margin-left: 10px;"},
+    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "10_cervicalAROM", "style": "margin-left: 10px;"},
+    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "11_shoulderAROM", "style": "margin-left: 10px;"},
+    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "12_shoulderPROM", "style": "margin-left: 10px;"},
+    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "13_elbowAROM", "style": "margin-left: 10px;"},
+    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "14_wristAROM", "style": "margin-left: 10px;"},
+    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "15_scapula", "style": "margin-left: 10px;"},
+    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "16_additionalComments", "style": "margin-left: 10px;"}
+]
+
