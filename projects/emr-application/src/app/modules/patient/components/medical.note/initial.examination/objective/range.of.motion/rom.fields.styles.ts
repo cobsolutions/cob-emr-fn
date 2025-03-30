25f5e836e130acf26a407155b7135ae84fea6168
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-03-30 03:43:04 +0200

    Range of Motion : No Limitations Noted, Cervical AROM

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/range.of.motion/rom.fields.styles.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/range.of.motion/rom.fields.styles.ts
@@ -1,13 +1,191 @@
 import { FieldControlStyles } from "../../../filed.control.style.selector/field.control.style";
 
 export var ROMFormStyles: FieldControlStyles[] = [
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "9_noLimitationsNoted", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "10_cervicalAROM", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "11_shoulderAROM", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "12_shoulderPROM", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "13_elbowAROM", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "14_wristAROM", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "15_scapula", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "16_additionalComments", "style": "margin-left: 10px;"}
+    { "label_style": "white-space: nowrap; min-width: 500px;", "name": "9_noLimitationsNoted", "style": "margin-left: 10px;" },
+    { "label_style": "white-space: nowrap; min-width: 500px;", "name": "10_cervicalAROM", "style": "margin-left: 10px;" },
+    { "label_style": "white-space: nowrap; min-width: 500px;", "name": "11_shoulderAROM", "style": "margin-left: 10px;" },
+    { "label_style": "white-space: nowrap; min-width: 500px;", "name": "12_shoulderPROM", "style": "margin-left: 10px;" },
+    { "label_style": "white-space: nowrap; min-width: 500px;", "name": "13_elbowAROM", "style": "margin-left: 10px;" },
+    { "label_style": "white-space: nowrap; min-width: 500px;", "name": "14_wristAROM", "style": "margin-left: 10px;" },
+    { "label_style": "white-space: nowrap; min-width: 500px;", "name": "15_scapula", "style": "margin-left: 10px;" },
+    { "label_style": "white-space: nowrap; min-width: 500px;", "name": "16_additionalComments", "style": "margin-left: 10px;" },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 10px;",
+        "name": "34_aROM",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 10px;",
+        "name": "35_pROM",
+        "style": "margin-left: 10px;"
+    },
+    //33
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "36_cervical",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "37_thoracic",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "38_shoulder",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "39_elbow",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "40_wrist",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "41_hand",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "42_lumbar",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "43_hip",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "44_knee",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "45_ankle",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "46_feet",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "12_pROMtextarea",
+        "style": "margin-left: 10px;"
+    },
+
+    //PROM
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "0_cervical",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "1_thoracic",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "2_shoulder",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "3_elbow",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "4_wrist",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "5_hand",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "6_lumbar",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "7_hip",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "8_knee",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "9_ankle",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "10_feet",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "11_pROMtextarea",
+        "style": "margin-left: 10px;"
+    },
+
+
+    //Cervical AROM
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "13_applyToAll",
+        "style": "margin-left: 10px;width:150px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "14_forwardBending",
+        "style": "margin-left: 10px;width:150px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "15_backwardBending",
+        "style": "margin-left: 10px;width:150px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "16_rightRotation",
+        "style": "margin-left: 10px;width:150px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "17_leftRotation",
+        "style": "margin-left: 10px;width:150px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "18_rightSideBending",
+        "style": "margin-left: 10px;width:150px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "19_leftSideBending",
+        "style": "margin-left: 10px;width:150px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 20px;",
+        "name": "20_cervicalAROMComments",
+        "style": "margin-left: 10px;width:250px"
+    }
+
+
 ]
 
