c299ce5cf88914b2a8d723d8fbd37055601a9368
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-04-03 15:08:49 +0200

    add No Limitations Noted in strength section

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/strength/strength.fields.styles.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/strength/strength.fields.styles.ts
@@ -1,14 +1,19 @@
 import { FieldControlStyles } from "../../../filed.control.style.selector/field.control.style";
 
 export var StrengthFormStyles: FieldControlStyles[] = [
-   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "11_noLimitationsNoted", "style": "margin-left: 10px;"},
-   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "12_selectiveTissueTensionUpper", "style": "margin-left: 10px;"},
-   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "13_gripPinch", "style": "margin-left: 10px;"},
-   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "14_grossMuscleTestsUpper", "style": "margin-left: 10px;"},
-   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "15_redcordNeuracStabilityTests", "style": "margin-left: 10px;"},
-   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "16_grossMuscleTestsTrunk", "style": "margin-left: 10px;"},
-   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "17_grossMuscleTestsLower", "style": "margin-left: 10px;"},
-   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "18_coreStrength", "style": "margin-left: 10px;"},
-   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "19_manualMuscleTests", "style": "margin-left: 10px;"},
-   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "20_additionalComments", "style": "margin-left: 10px;"}
+   { "label_style": "white-space: nowrap; min-width: 500px;", "name": "32_additionalComments", "style": "margin-left: 10px;" },
+   { "label_style": "white-space: nowrap; min-width: 500px;", "name": "31_manualMuscleTests", "style": "margin-left: 10px;" },
+   { "label_style": "white-space: nowrap; min-width: 500px;", "name": "30_coreStrength", "style": "margin-left: 10px;" },
+   { "label_style": "white-space: nowrap; min-width: 500px;", "name": "29_grossMuscleTestsLower", "style": "margin-left: 10px;" },
+   { "label_style": "white-space: nowrap; min-width: 500px;", "name": "28_grossMuscleTestsTrunk", "style": "margin-left: 10px;" },
+   { "label_style": "white-space: nowrap; min-width: 500px;", "name": "27_redcordNeuracStabilityTests", "style": "margin-left: 10px;" },
+   { "label_style": "white-space: nowrap; min-width: 500px;", "name": "26_grossMuscleTestsUpper", "style": "margin-left: 10px;" },
+   { "label_style": "white-space: nowrap; min-width: 500px;", "name": "25_gripPinch", "style": "margin-left: 10px;" },
+   { "label_style": "white-space: nowrap; min-width: 500px;", "name": "24_selectiveTissueTensionUpper", "style": "margin-left: 10px;" },
+   { "label_style": "white-space: nowrap; min-width: 500px;", "name": "23_noLimitationsNoted", "style": "margin-left: 10px;" },
+   {
+      "label_style": "white-space: nowrap; min-width: 500px;",
+      "name": "34_23noLimitationsNotedcheckbox",
+      "style": "margin-left: 10px;"
+  }
 ]
