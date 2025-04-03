caf5af251eee0825b3d642a632331bdb0a4d1265
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-04-03 19:51:39 +0200

    first impl of grip component

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/strength/components/grip/grip.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/strength/components/grip/grip.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-grip',
+  selector: 'grip',
   templateUrl: './grip.component.html',
   styleUrls: ['./grip.component.css']
 })
 export class GripComponent implements OnInit {
+  gripStrengthForm: FormGroup;
+  
+  // Grip types
+  gripTypes = ['Power Grip', 'Lateral Pinch', 'Tip Pinch/Pincer', 'Tripod Pinch'];
 
-  constructor() { }
+  // Mapping grip types to form control-safe names (removing spaces and special characters)
+  gripControlMap = {
+    'Power Grip': 'powerGrip',
+    'Lateral Pinch': 'lateralPinch',
+    'Tip Pinch/Pincer': 'tipPinch',
+    'Tripod Pinch': 'tripodPinch'
+  };
 
-  ngOnInit(): void {
+  // Test options specific to each grip type
+  testOptionsMap = {
+    'Power Grip': ['Not Tested', 'Weak', 'Moderate', 'Strong'],
+    'Lateral Pinch': ['Not Tested', 'Poor', 'Fair', 'Good'],
+    'Tip Pinch/Pincer': ['Not Tested', 'Minimal', 'Partial', 'Full'],
+    'Tripod Pinch': ['Not Tested', 'Insufficient', 'Satisfactory', 'Excellent']
+  };
+
+  constructor(private fb: FormBuilder) {
   }
-
+  ngOnInit(): void {
+    this.gripStrengthForm = this.fb.group({
+      right: this.fb.group({
+        powerGrip: ['Not Tested'],
+        lateralPinch: ['Not Tested'],
+        tipPinch: ['Not Tested'],
+        tripodPinch: ['Not Tested'],
+      }),
+      left: this.fb.group({
+        powerGrip: ['Not Tested'],
+        lateralPinch: ['Not Tested'],
+        tipPinch: ['Not Tested'],
+        tripodPinch: ['Not Tested'],
+      }),
+    });
+  }
 }
