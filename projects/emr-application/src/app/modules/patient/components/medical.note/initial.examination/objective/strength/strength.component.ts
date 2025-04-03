d508272580d44c8254ae0abbde5887181816ae7d
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-04-03 14:54:41 +0200

    render first level of strength

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/strength/strength.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/strength/strength.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FieldDependentsService } from '../../../../../services/medical.note/field.dependents.builder/field-dependents.service';
+import { FieldControlStyles } from '../../../filed.control.style.selector/field.control.style';
+import { StrengthFormStyles } from './strength.fields.styles';
 
 @Component({
-  selector: 'app-strength',
+  selector: 'strength',
   templateUrl: './strength.component.html',
   styleUrls: ['./strength.component.css']
 })
 export class StrengthComponent implements OnInit {
 
-  constructor() { }
+  strengthForm: FormGroup;
+  @Output() formReady = new EventEmitter<FormGroup>();
+  @Input() fields: any
+  styles: FieldControlStyles[] = StrengthFormStyles;
+  constructor(private fb: FormBuilder
+    , private fieldDependentsService: FieldDependentsService) { }
 
   ngOnInit(): void {
+    this.fields = this.fieldDependentsService.buildHierarchyRecursive(this.fields);
+    this.strengthForm = this.fb.group({});
+    this.formReady.emit(this.strengthForm);
   }
 
+  getstyleFieldControl(fieldName: string): FieldControlStyles {
+    return this.styles.find(obj => obj.name === fieldName);
+  }
 }
---
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/strength/strength.fields.styles.ts
@@ -0,0 +1,14 @@
+import { FieldControlStyles } from "../../../filed.control.style.selector/field.control.style";
+
+export var StrengthFormStyles: FieldControlStyles[] = [
+   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "11_noLimitationsNoted", "style": "margin-left: 10px;"},
+   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "12_selectiveTissueTensionUpper", "style": "margin-left: 10px;"},
+   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "13_gripPinch", "style": "margin-left: 10px;"},
+   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "14_grossMuscleTestsUpper", "style": "margin-left: 10px;"},
+   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "15_redcordNeuracStabilityTests", "style": "margin-left: 10px;"},
+   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "16_grossMuscleTestsTrunk", "style": "margin-left: 10px;"},
+   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "17_grossMuscleTestsLower", "style": "margin-left: 10px;"},
+   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "18_coreStrength", "style": "margin-left: 10px;"},
+   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "19_manualMuscleTests", "style": "margin-left: 10px;"},
+   {"label_style": "white-space: nowrap; min-width: 500px;", "name": "20_additionalComments", "style": "margin-left: 10px;"}
+]
