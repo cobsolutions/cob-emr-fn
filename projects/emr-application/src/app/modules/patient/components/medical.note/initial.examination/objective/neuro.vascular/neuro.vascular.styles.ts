9189e0138cafc94777169caeae0dda2c612925ac
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-04-06 20:23:46 +0200

    add level of Complaints of any radicular symptoms in either extremity

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/neuro.vascular/neuro.vascular.styles.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/neuro.vascular/neuro.vascular.styles.ts
@@ -12,5 +12,31 @@ export var NeuroVascularStyles: FieldControlStyles[] = [
     {"label_style": "white-space: nowrap; min-width: 500px;", "name": "65_semmesWeinsteinUpper", "style": "margin-left: 10px;"},
     {"label_style": "white-space: nowrap; min-width: 500px;", "name": "66_tinelsUpper", "style": "margin-left: 10px;"},
     {"label_style": "white-space: nowrap; min-width: 500px;", "name": "67_transverseLigamentStability", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "68_additionalComments", "style": "margin-left: 10px;"}
+    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "68_additionalComments", "style": "margin-left: 10px;"},
+
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "72_complaintsOfAnyRadicularSymptomsInEitherExtremitytext",
+        "style": "margin-left: 20px;width:200px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "73_extremityReflexesAreEqualAndNormalBilateral",
+        "style": "margin-left: 20px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "74_sensoryOrVascularDeficitsNoted",
+        "style": "margin-left: 20px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "75_73extremityReflexesAreEqualAndNormalBilateraltext",
+        "style": "margin-left: 20px;width:200px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "76_74sensoryOrVascularDeficitsNotedtext",
+        "style": "margin-left: 10px;width:200px"
+    }
 ]
