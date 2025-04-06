753a9d861413232094cdcfde3316850ac73cc3ba
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-04-06 20:02:40 +0200

    add neuroVascular style file can create fromGroup

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/neuro.vascular/neuro-vascular.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/neuro.vascular/neuro-vascular.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { FieldDependentsService } from '../../../../../services/medical.note/field.dependents.builder/field-dependents.service';
+import { FieldControlStyles } from '../../../filed.control.style.selector/field.control.style';
+import { NeuroVascularStyles } from './neuro.vascular.styles';
 
 @Component({
   selector: 'app-neuro-vascular',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./neuro-vascular.component.css']
 })
 export class NeuroVascularComponent implements OnInit {
-
-  constructor() { }
+  neuroVascularForm: FormGroup;
+  @Output() formReady = new EventEmitter<FormGroup>();
+  @Input() fields: any
+  styles: FieldControlStyles[] = NeuroVascularStyles;
+  constructor(private fb: FormBuilder
+    , private fieldDependentsService: FieldDependentsService) { }
 
   ngOnInit(): void {
+    this.fields = this.fieldDependentsService.buildHierarchyRecursive(this.fields);
+    this.neuroVascularForm = this.fb.group({});
+    this.formReady.emit(this.neuroVascularForm);
+  }
+  getstyleFieldControl(fieldName: string): FieldControlStyles {
+    return this.styles.find(obj => obj.name === fieldName);
   }
 
 }
