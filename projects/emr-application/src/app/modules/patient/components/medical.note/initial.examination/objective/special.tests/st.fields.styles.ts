7669f68c1758750ce6f0dd0ca252e7b550d191f6
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-04-07 21:08:53 +0200

    add special test in the objective component and add it's first level of components

---
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/special.tests/st.fields.styles.ts
@@ -0,0 +1,100 @@
+import { FieldControlStyles } from "../../../filed.control.style.selector/field.control.style";
+
+export var SpecialTestStyles: FieldControlStyles[] =[
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "182_passiveJointMobilityShoulder",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "183_cervicalQuadrant",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "184_cervicalCompDist",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "185_sCJoint",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "186_aCJoint",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "187_capsularPattern",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "188_impingement",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "189_gHJStability",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "190_labrum",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "191_rotatorCuff",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "192_speedsTest",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "193_yergasonsTest",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "194_ligamentIntegrityElbow",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "195_spurlingsManeuver",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "196_alarLigamentTest",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "197_alarLigamentStress",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "198_workConditioning",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "199_tMRFAB4Worksheet",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "200_additionalComments",
+        "style": "margin-left: 30px;"
+    }
+]
+
