7dd79de7966a306704f621cba01c1ab567e1d1a0
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-04-07 16:37:28 +0200

    add basic struct of neuroVascularRightLeft

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/neuro.vascular/components/neuro.vascular.right.left/neuro-vascular-right-left.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/neuro.vascular/components/neuro.vascular.right.left/neuro-vascular-right-left.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
+interface SelectValue {
+  view: string,
+  val: string
+}
 @Component({
   selector: 'app-neuro-vascular-right-left',
   templateUrl: './neuro-vascular-right-left.component.html',
   styleUrls: ['./neuro-vascular-right-left.component.css']
 })
 export class NeuroVascularRightLeftComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup
+  @Input() parentForm: FormGroup;
+  @Input() parentFieldName: string;
+  @Input() testStyle: string
+  selectValues: SelectValue[]
+  @Input() topSelect: string[];
+  @Input() bodySelect: string[]
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-  }
+    this.form = this.fb.group({});
+    if (this.topSelect)
+      this.topSelect.forEach(field => {
+        this.form.addControl(`${this.toCamelCase(field)}`, new FormControl(this.selectValues[0].val));
+        this.form.get(`${this.toCamelCase(field)}`).valueChanges.subscribe(v => {
+          this.parentForm.get(this.parentFieldName).setValue(this.form.value);
+        })
+      })
 
+    if (this.bodySelect)
+      this.bodySelect.forEach(field => {
+        this.form.addControl(`right_${this.toCamelCase(field)}`, new FormControl(this.selectValues[0].val));
+        this.form.addControl(`left_${this.toCamelCase(field)}`, new FormControl(this.selectValues[0].val));
+
+        this.form.get(`right_${this.toCamelCase(field)}`).valueChanges.subscribe(v => {
+          this.parentForm.get(this.parentFieldName).setValue(this.form.value);
+        })
+        this.form.get(`left_${this.toCamelCase(field)}`).valueChanges.subscribe(v => {
+          this.parentForm.get(this.parentFieldName).setValue(this.form.value);
+        })
+      });
+  }
+  toCamelCase(value: string): string {
+    return value.replace(/\s+(.)/g, (match, group1) => group1.toUpperCase());
+  }
 }
