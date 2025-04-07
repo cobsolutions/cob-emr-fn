8d89fb992f66541cfce5f00cffe6d8c8eead7b24
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-04-07 08:57:10 +0200

    finalize Cranial Nerve Screen

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/neuro.vascular/neuro.vascular.styles.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/neuro.vascular/neuro.vascular.styles.ts
@@ -1,42 +1,289 @@
 import { FieldControlStyles } from "../../../filed.control.style.selector/field.control.style";
 
 export var NeuroVascularStyles: FieldControlStyles[] = [
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "57_complaintsOfAnyRadicularSymptomsInEitherExtremity", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "58_cranialNerveScreen", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "59_myotomesUpper", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "60_dermatomesUpper", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "61_upperReflexes", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "62_neuralTissueTensionUpper", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "63_vertebralArtery", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "64_thoracicOutlet", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "65_semmesWeinsteinUpper", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "66_tinelsUpper", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "67_transverseLigamentStability", "style": "margin-left: 10px;"},
-    {"label_style": "white-space: nowrap; min-width: 500px;", "name": "68_additionalComments", "style": "margin-left: 10px;"},
+	{ "label_style": "white-space: nowrap; min-width: 500px;", "name": "57_complaintsOfAnyRadicularSymptomsInEitherExtremity", "style": "margin-left: 10px;" },
+	{ "label_style": "white-space: nowrap; min-width: 500px;", "name": "58_cranialNerveScreen", "style": "margin-left: 10px;" },
+	{ "label_style": "white-space: nowrap; min-width: 500px;", "name": "59_myotomesUpper", "style": "margin-left: 10px;" },
+	{ "label_style": "white-space: nowrap; min-width: 500px;", "name": "60_dermatomesUpper", "style": "margin-left: 10px;" },
+	{ "label_style": "white-space: nowrap; min-width: 500px;", "name": "61_upperReflexes", "style": "margin-left: 10px;" },
+	{ "label_style": "white-space: nowrap; min-width: 500px;", "name": "62_neuralTissueTensionUpper", "style": "margin-left: 10px;" },
+	{ "label_style": "white-space: nowrap; min-width: 500px;", "name": "63_vertebralArtery", "style": "margin-left: 10px;" },
+	{ "label_style": "white-space: nowrap; min-width: 500px;", "name": "64_thoracicOutlet", "style": "margin-left: 10px;" },
+	{ "label_style": "white-space: nowrap; min-width: 500px;", "name": "65_semmesWeinsteinUpper", "style": "margin-left: 10px;" },
+	{ "label_style": "white-space: nowrap; min-width: 500px;", "name": "66_tinelsUpper", "style": "margin-left: 10px;" },
+	{ "label_style": "white-space: nowrap; min-width: 500px;", "name": "67_transverseLigamentStability", "style": "margin-left: 10px;" },
+	{ "label_style": "white-space: nowrap; min-width: 500px;", "name": "68_additionalComments", "style": "margin-left: 10px;" },
 
-    {
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "72_complaintsOfAnyRadicularSymptomsInEitherExtremitytext",
+		"style": "margin-left: 20px;width:200px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "73_extremityReflexesAreEqualAndNormalBilateral",
+		"style": "margin-left: 20px;"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "74_sensoryOrVascularDeficitsNoted",
+		"style": "margin-left: 20px;"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "75_73extremityReflexesAreEqualAndNormalBilateraltext",
+		"style": "margin-left: 20px;width:200px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "76_74sensoryOrVascularDeficitsNotedtext",
+		"style": "margin-left: 10px;width:200px"
+	}
+
+	,
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "107_unilateralSmellCN1",
+		"style": "margin-left: 30px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "108_confrontationCN2",
+		"style": "margin-left: 30px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "109_hallPikeCN8Coch",
+		"style": "margin-left: 30px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "110_fingerRustleCN8Coch",
+		"style": "margin-left: 30px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "111_boneConductionCN8Coch",
+		"style": "margin-left: 30px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "112_convergenceCN346",
+		"style": "margin-left: 30px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "113_facialSensationCN5",
+		"style": "margin-left: 30px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "114_jawJerkClonusCN5",
+		"style": "margin-left: 30px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "115_smileCN7",
+		"style": "margin-left: 30px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "116_frownCN7",
+		"style": "margin-left: 30px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "117_bodyTiltCN8Vest",
+		"style": "margin-left: 30px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "118_swallowingCN9",
+		"style": "margin-left: 30px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "119_uvulaTestCN10",
+		"style": "margin-left: 30px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "120_trapeziusSCMCN11",
+		"style": "margin-left: 30px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "121_tongueProtrusionCN12",
+		"style": "margin-left: 30px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "126_unilateralSmellCN1positiveselection",
+		"style": "margin-left: 50px;height:50px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "127_unilateralSmellCN1positivecomment",
+		"style": "margin-left: 50px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "128_confrontationCN2positiveselection",
+		"style": "margin-left: 50px;height:50px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "129_confrontationCN2positivecomment",
+		"style": "margin-left: 50px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "132_fingerRustleCN8Cochpositiveselection",
+		"style": "margin-left: 50px;height:50px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "133_fingerRustleCN8Cochpositivecomment",
+		"style": "margin-left: 50px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "134_boneConductionCN8Cochpositiveselection",
+		"style": "margin-left: 50px;height:50px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "135_boneConductionCN8Cochpositivecomment",
+		"style": "margin-left: 50px;width:150px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "136_convergenceCN346positiveselection",
+		"style": "margin-left: 50px;height:50px"
+	},
+	{
+		"label_style": "white-space: nowrap; min-width: 500px;",
+		"name": "137_convergenceCN346positivecomment",
+		"style": "margin-left: 50px;width:150px"
+	},
+	{
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "72_complaintsOfAnyRadicularSymptomsInEitherExtremitytext",
-        "style": "margin-left: 20px;width:200px"
+        "name": "138_facialSensationCN5positiveselection",
+        "style": "margin-left: 50px;height:50px"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "73_extremityReflexesAreEqualAndNormalBilateral",
-        "style": "margin-left: 20px;"
+        "name": "139_facialSensationCN5positivecomment",
+        "style": "margin-left: 50px;width:150px"
+    },
+	{
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "140_jawJerkClonusCN5positiveselection",
+        "style": "margin-left: 50px;height:50px"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "74_sensoryOrVascularDeficitsNoted",
-        "style": "margin-left: 20px;"
+        "name": "141_jawJerkClonusCN5positivecomment",
+        "style": "margin-left: 50px;width:150px"
+    },
+	{
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "142_smileCN7positiveselection",
+        "style": "margin-left: 50px;height:50px"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "75_73extremityReflexesAreEqualAndNormalBilateraltext",
-        "style": "margin-left: 20px;width:200px"
+        "name": "143_smileCN7positivecomment",
+        "style": "margin-left: 50px;width:150px"
+    },
+	{
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "144_frownCN7positiveselection",
+        "style": "margin-left: 50px;height:50px"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "76_74sensoryOrVascularDeficitsNotedtext",
-        "style": "margin-left: 10px;width:200px"
+        "name": "145_frownCN7positivecomment",
+        "style": "margin-left: 50px;width:150px"
+    },
+	{
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "146_bodyTiltCN8Vestpositiveselection",
+        "style": "margin-left: 50px;height:50px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "147_bodyTiltCN8Vestpositivecomment",
+        "style": "margin-left: 50px;width:150px"
+    },
+	{
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "148_hallPikeCN8Cochpositiveselection",
+        "style": "margin-left: 50px;height:50px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "149_hallPikeCN8Cochpositivecomment",
+        "style": "margin-left: 50px;width:150px"
+    },
+	{
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "150_fingerRustleCN8Cochpositiveselection",
+        "style": "margin-left: 50px;height:70px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "151_fingerRustleCN8Cochpositivecomment",
+        "style": "margin-left: 50px;width:150px"
+    },
+	{
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "152_boneConductionCN8Cochpositiveselection",
+        "style": "margin-left: 50px;height:50px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "153_boneConductionCN8Cochpositivecomment",
+        "style": "margin-left: 50px;width:150px"
+    },
+	{
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "154_swallowingCN9positiveselection",
+        "style": "margin-left: 50px;height:50px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "155_swallowingCN9positivecomment",
+        "style": "margin-left: 50px;width:150px"
+    },
+	{
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "156_uvulaTestCN10positiveselection",
+        "style": "margin-left: 50px;height:50px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "157_uvulaTestCN10positivecomment",
+        "style": "margin-left: 50px;width:150px"
+    },
+	{
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "158_trapeziusSCMCN11positiveselection",
+        "style": "margin-left: 50px;height:50px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "159_trapeziusSCMCN11positivecomment",
+        "style": "margin-left: 50px;width:150px"
+    },
+	{
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "160_tongueProtrusionCN12positiveselection",
+        "style": "margin-left: 50px;height:50px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "161_tongueProtrusionCN12positivecomment",
+        "style": "margin-left: 50px;width:150px"
     }
 ]
