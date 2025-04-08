0aa66a9eb294ed002618b5f3a13f310c366216c5
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-04-08 17:40:04 +0200

    finalize Palpation

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/palpation/palpation.fields.styles.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/objective/palpation/palpation.fields.styles.ts
@@ -3,213 +3,149 @@ import { FieldControlStyles } from "../../../filed.control.style.selector/field.
 export var PalpationStyles: FieldControlStyles[] = [
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "182_passiveJointMobilityShoulder",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "183_cervicalQuadrant",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "184_cervicalCompDist",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "185_sCJoint",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "186_aCJoint",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "187_capsularPattern",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "188_impingement",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "189_gHJStability",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "190_labrum",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "191_rotatorCuff",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "192_speedsTest",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "193_yergasonsTest",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "194_ligamentIntegrityElbow",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "195_spurlingsManeuver",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "196_alarLigamentTest",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "197_alarLigamentStress",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "198_workConditioning",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "199_tMRFAB4Worksheet",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "200_additionalComments",
-        "style": "margin-left: 30px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "201_182passiveJointMobilityShoulderspecialtestsrightleftselect",
-        "style": "margin-left: 200px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "202_183cervicalQuadrantspecialtestsrightleftselect",
-        "style": "margin-left: 200px;"
-    },
-    {
-        "label_style": "margin-left: 30px;white-space: nowrap; min-width: 500px;",
-        "name": "203_compression",
-        "style": "margin-left: 10px;width:200px"
-    },
-    {
-        "label_style": "margin-left: 30px;white-space: nowrap; min-width: 500px;",
-        "name": "204_distraction",
-        "style": "margin-left: 10px;width:200px"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "205_187capsularPatternspecialtestsrightleftselect",
-        "style": "margin-left: 200px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "206_188impingementspecialtestsrightleftselect",
-        "style": "margin-left: 200px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "207_189gHJStabilityspecialtestsrightleftselect",
-        "style": "margin-left: 200px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "209_191rotatorCuffspecialtestsrightleftselect",
-        "style": "margin-left: 200px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "210_190labrumspecialtestsrightleftselect",
-        "style": "margin-left: 200px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "211_192speedsTestspecialtestsrightleftselect",
-        "style": "margin-left: 200px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "213_193yergasonsTestspecialtestsrightleftselect",
-        "style": "margin-left: 200px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "214_193yergasonsTestspecialtestsrightleftselect",
-        "style": "margin-left: 200px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "215_195spurlingsManeuverspecialtestsrightleftselect",
-        "style": "margin-left: 200px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "216_196alarLigamentTestspecialtestsrightleftselect",
-        "style": "margin-left: 200px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "217_197alarLigamentStresstextArea",
-        "style": "margin-left: 30px; width:400px"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "218_185sCJointspecialtestsrightleftcheck",
+        "name": "256_palpation",
         "style": "margin-left: 10px;"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "219_186aCJointspecialtestsrightleftcheck",
-        "style": "margin-left: 100px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 50px;",
-        "name": "220_materialHandlingBilateralLifting",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 50px;",
-        "name": "221_nonMaterialHandling",
+        "name": "257_additionalComments",
         "style": "margin-left: 10px;"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "224_221nonMaterialHandlingmaterialhandling",
-        "style": "margin-left: 400px;"
+        "name": "284_rightSCM",
+        "style": "margin-left: 30px;"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "225_220materialHandlingBilateralLiftingmaterialhandling",
-        "style": "margin-left: 400px;"
+        "name": "285_leftSCM",
+        "style": "margin-left: 30px;"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "226_199tMRFAB4WorksheettextArea",
-        "style": "margin-left: 10px;width: 500px;"
+        "name": "286_rightScalenes",
+        "style": "margin-left: 30px;"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "227_200additionalCommentstextArea",
-        "style": "margin-left: 10px;width: 600px;"
+        "name": "287_leftScalenes",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "288_rightSuboccipitalMyofascia",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "289_leftSuboccipitalMyofascia",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "290_rightCervicalParaspinals",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "291_leftCervicalParaspinals",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "292_rightLevatorScapula",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "293_leftLevatorScapula",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "294_rightUpperTrapezius",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "295_leftUpperTrapezius",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "296_rightMiddleTrapezius",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "297_leftMiddleTrapezius",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "298_rightLowerTrapezius",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "299_leftLowerTrapezius",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "300_rightRotatorCuffInsertionAtGreaterTubercle",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "301_leftRotatorCuffInsertionAtGreaterTubercle",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "302_rightRotatorCuffInsertionAtLesserTubercle",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "303_leftRotatorCuffInsertionAtLesserTubercle",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "304_rightPosteriorCuff",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "305_leftPosteriorCuff",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "306_rightBicepsShortHead",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "307_leftBicepsShortHead",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "308_rightBicepsLongHead",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "309_leftBicepsLongHead",
+        "style": "margin-left: 30px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "310_257additionalCommentstextArea",
+        "style": "margin-left: 10px;width: 300px;"
     }
 ]
+
 
