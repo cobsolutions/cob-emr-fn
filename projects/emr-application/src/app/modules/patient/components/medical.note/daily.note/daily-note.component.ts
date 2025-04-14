bb0ae20512a5f9ad8d8bdd109e2a0f079a92ee1c
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-04-14 16:20:07 +0200

    create daily note stepper and handle return to patient records action

--- projects/emr-application/src/app/modules/patient/components/medical.note/daily.note/daily-note.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/daily.note/daily-note.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { StepperSelectionEvent } from '@angular/cdk/stepper';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'daily-note',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DailyNoteComponent implements OnInit {
 
-  constructor() { }
+  stepperOrientation: 'horizontal' | 'vertical' = 'horizontal';
+  activeStepIndex: number;
+  dailyNoteForm: FormGroup
+  visitedSteps: boolean[] = [];
+  @Input() caseId: number
+  @Output() back = new EventEmitter<void>();
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.visitedSteps = [true, false, false, false]
+    this.dailyNoteForm = this.fb.group({
+      subjective: this.fb.group({}),
+      objective: this.fb.group({}),
+      assessment: this.fb.group({}),
+      planOfCare: this.fb.group({}),
+    });
+  }
+  soapActions(action: string) {
+    if (action === 'back')
+      this.backtoPatientRecordActions()
+    if (action === 'draft')
+      this.draft();
+  }
+  backtoPatientRecordActions() {
+    this.back.emit();
   }
 
+  private draft() {
+
+  }
+  onStepChange(event: StepperSelectionEvent): void {
+    this.activeStepIndex = event.selectedIndex;
+    event.selectedIndex
+    this.visitedSteps[event.selectedIndex] = true;
+  }
+  setChildForm(section: string, formGroup: FormGroup) {
+    this.dailyNoteForm.setControl(section, formGroup);
+  }
 }
