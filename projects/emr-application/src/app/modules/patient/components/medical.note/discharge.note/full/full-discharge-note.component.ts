527ab9f631ab126caa5e48bd53b068e624313783
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-04-15 20:56:16 +0200

    create skeleton of quick and full discharge component

---
+++ projects/emr-application/src/app/modules/patient/components/medical.note/discharge.note/full/full-discharge-note.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-full-discharge-note',
+  templateUrl: './full-discharge-note.component.html',
+  styleUrls: ['./full-discharge-note.component.css']
+})
+export class FullDischargeNoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/emr-application/src/app/modules/patient/components/medical.note/discharge.note/quick/quick-discharge-note.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'quick-discharge-note',
+  templateUrl: './quick-discharge-note.component.html',
+  styleUrls: ['./quick-discharge-note.component.css']
+})
+export class QuickDischargeNoteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
