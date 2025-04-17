c0cdaeee9ed764b4cdb42e7c8625ff9b6f7487ad
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-04-17 16:56:55 +0200

    add proxy file env prod and kc prod

--- projects/emr-application/src/environments/environment.prod.ts
+++ projects/emr-application/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+import keycloakConfigProd from "./keycloak.config.prod";
+
 export const environment = {
-  production: true
+  production: true,
+  baseURL:"/emr/api",
+  keycloak: keycloakConfigProd,
 };
