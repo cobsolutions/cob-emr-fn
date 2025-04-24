0d2deb10b8c0e5da6f15b9fc804c6dcf2cd809ee
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-04-24 12:16:33 +0200

    add styles for general profile , Inspection section

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/inspection/inspection.fields.styles.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/inspection/inspection.fields.styles.ts
@@ -3,37 +3,12 @@ import { FieldControlStyles } from "../../../filed.control.style.selector/field.
 export var InspectionFormStyles: FieldControlStyles[] = [
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_0ee1b4d8-a635-4018-a784-2226c08d58f7_ddddd",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_test",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_0ee1b4d8-a635-4018-a784-2226c08d58f7",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_984a59fb-6401-4081-b6c6-2ae6aeb4b018_patientparentguardianConcent",
+        "name": "inspection_patientConsent",
         "style": "margin-left: 10px;"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
         "name": "inspection_chaperone",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_9c32f491-0a7e-4b3a-92d6-f0ffe03954b4",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_9c32f491-0a7e-4b3a-92d6-f0ffe03954b4",
         "style": "margin-left: 10px;"
     },
     {
@@ -44,11 +19,11 @@ export var InspectionFormStyles: FieldControlStyles[] = [
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
         "name": "inspection_girthMeasurementUpper",
-        "style": "margin-left: 60px;"
+        "style": "margin-left: 10px;"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_23409f43-3e82-4e01-91a7-4b02fccc108c",
+        "name": "inspection_girthMeasurementLower",
         "style": "margin-left: 10px;"
     },
     {
@@ -58,127 +33,12 @@ export var InspectionFormStyles: FieldControlStyles[] = [
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_4f8c6a53-a35a-4901-aa2a-7e8449946fef_incisionSites",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_4f8c6a53-a35a-4901-aa2a-7e8449946fef_surgicalPrecautions",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_4f8c6a53-a35a-4901-aa2a-7e8449946fef_scarMobility",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_4f8c6a53-a35a-4901-aa2a-7e8449946fef_scarTybe",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_4f8c6a53-a35a-4901-aa2a-7e8449946fef_woundDescription",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_4f8c6a53-a35a-4901-aa2a-7e8449946fef_woundMeasurements",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_28924475-0443-419d-8d43-d5a2669bdcaa",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_28924475-0443-419d-8d43-d5a2669bdcaa_compliantWithRomRistrictions",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_40a57fab-4492-4c79-94f4-c18d38bc6417",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_a5bcdb83-158d-47a8-986b-05b02f1151b2",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_3e9f8f5e-c470-4656-81b6-8149f47b3fc4_woundDescriptionText",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_10ad4fb3-c35b-4e09-90c5-7b1c2ca8acdb_length",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_10ad4fb3-c35b-4e09-90c5-7b1c2ca8acdb_width",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
         "name": "inspection_woundCare",
         "style": "margin-left: 10px;"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_432e431a-9115-4e4d-b3c9-5db9c30c559b_surfaceCultureUsedToExudate",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_493602cd-2aca-4077-b396-2a68486bf19d_techniqueUtilized",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_493602cd-2aca-4077-b396-2a68486bf19d_levine",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_493602cd-2aca-4077-b396-2a68486bf19d_deepSwab",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_493602cd-2aca-4077-b396-2a68486bf19d_semiquantitativeSwab",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_493602cd-2aca-4077-b396-2a68486bf19d_quantitativeSwab",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_493602cd-2aca-4077-b396-2a68486bf19d_provideMedicalReasoningForUseOfWoundSurfaceTechnique",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_a5b14878-afc3-4b7f-b2fd-67f2b74f95ee",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_ea58d946-aa99-4ac0-8f67-4f7a9caf7902",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_bb9592c8-8baa-4ef5-9906-c1712c967ffc",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_8ae4a3b9-d048-4f6d-ae82-73739319f1b0",
+        "name": "inspection_surgicalScarring",
         "style": "margin-left: 10px;"
     },
     {
@@ -188,37 +48,27 @@ export var InspectionFormStyles: FieldControlStyles[] = [
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_9927be39-9207-44ad-a473-39a047e18c7b_weight",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_9927be39-9207-44ad-a473-39a047e18c7b_height",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_9927be39-9207-44ad-a473-39a047e18c7b_unites",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_9927be39-9207-44ad-a473-39a047e18c7b_index",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_9927be39-9207-44ad-a473-39a047e18c7b_followupPlan",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
         "name": "inspection_additionalComments",
         "style": "margin-left: 10px;"
     },
     {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 150px;",
+        "name": "inspection_1090dee0-1201-400a-9cf0-d450797dd5b4_patientparentguardianConsent",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 150px;",
+        "name": "inspection_829a7f5f-daf8-495f-b307-ea622e46abe3_chaperonePresent",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;margin-left: 150px;",
+        "name": "inspection_829a7f5f-daf8-495f-b307-ea622e46abe3",
+        "style": "margin-left: 10px;"
+    },
+    {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "inspection_b67f0eff-e479-4483-8e81-b1e9c38834c9",
+        "name": "inspection_bd471778-6055-4366-9895-4ac73c15f3c7",
         "style": "margin-left: 10px;"
     }
 ]
