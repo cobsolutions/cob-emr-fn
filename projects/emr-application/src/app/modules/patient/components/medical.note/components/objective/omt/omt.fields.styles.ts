73ba4f712f1a145374a4538f7c056043fb7290e3
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-07-08 16:53:48 +0300

    add depds of :Cerebral Concussion

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/omt/omt.fields.styles.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/omt/omt.fields.styles.ts
@@ -57,5 +57,25 @@ export var OMTFormStyles: FieldControlStyles[] = [
         "label_style": "white-space: nowrap; min-width: 500px;",
         "name": "omt_a78aac4c-396b-4ca4-9ddb-e5d8364564e4_score",
         "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 600px;",
+        "name": "omt_250c0114-ef5e-41cb-a31d-16e3dd2039f1_cerebralConcussion",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 660px;",
+        "name": "omt_f0d0ab87-f378-4c66-857f-64c393a963d9_lossOfConsciousness",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 660px;",
+        "name": "omt_f0d0ab87-f378-4c66-857f-64c393a963d9_posttraumaticAmnesia",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 660px;",
+        "name": "omt_f0d0ab87-f378-4c66-857f-64c393a963d9_postcerebralConcussionScale",
+        "style": "margin-left: 10px;"
     }
 ]
