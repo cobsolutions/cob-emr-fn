19881a1b16dd2f0bbc82f63f543bc85895ff982d
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-07-14 16:31:27 +0300

    add first level of Observation

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/observation/observation.fields.styles.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/observation/observation.fields.styles.ts
@@ -3,27 +3,37 @@ import { FieldControlStyles } from "../../../filed.control.style.selector/field.
 export var ObservationFormStyles: FieldControlStyles[] = [
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_indicateBodyType",
+        "name": "observation_IndicateBodyType",
         "style": "margin-left: 10px;"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_vitals",
+        "name": "observation_Vitals",
         "style": "margin-left: 10px;"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_adlManagement",
+        "name": "observation_AdlManagement",
         "style": "margin-left: 10px;"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_handedness",
+        "name": "observation_breathingAtRest",
         "style": "margin-left: 10px;"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_standingPosture",
+        "name": "observation_Transfers",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "observation_Castsplint",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "observation_StandingPosture",
         "style": "margin-left: 10px;"
     },
     {
@@ -33,7 +43,27 @@ export var ObservationFormStyles: FieldControlStyles[] = [
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_sixMinuteWalkTest",
+        "name": "observation_Scoliosis",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "observation_lowerExtremityStructure",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "observation_Gait",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "observation_MinuteWalkTest",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "observation_assistiveDevice",
         "style": "margin-left: 10px;"
     },
     {
@@ -48,6 +78,11 @@ export var ObservationFormStyles: FieldControlStyles[] = [
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "observation_muscleGuarding",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
         "name": "observation_muscleAtrophy",
         "style": "margin-left: 10px;"
     },
@@ -58,132 +93,12 @@ export var ObservationFormStyles: FieldControlStyles[] = [
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_scapulaPosition",
+        "name": "observation_apprehensionOfMovement",
         "style": "margin-left: 10px;"
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_4213152b-3ed3-4d6d-88e9-12c82e77acab",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_f27077a5-c37c-4f5c-ae70-f5f7582fa4ff_bloodPressure",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_f27077a5-c37c-4f5c-ae70-f5f7582fa4ff_temperature",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_f27077a5-c37c-4f5c-ae70-f5f7582fa4ff_pulse",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_f27077a5-c37c-4f5c-ae70-f5f7582fa4ff_respiration",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_f27077a5-c37c-4f5c-ae70-f5f7582fa4ff_weight",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_578cc2e0-5a86-4d65-9fd0-25ba5ed12361_bathing",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_578cc2e0-5a86-4d65-9fd0-25ba5ed12361_toileting",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_578cc2e0-5a86-4d65-9fd0-25ba5ed12361_dressing",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_578cc2e0-5a86-4d65-9fd0-25ba5ed12361_grooming",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_578cc2e0-5a86-4d65-9fd0-25ba5ed12361_eating",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_a741c523-080a-4cab-b1d1-ff425a4be337_handednessselect",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_fb2aa3a6-2036-407c-9f67-cd5121e724c3",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_fb2aa3a6-2036-407c-9f67-cd5121e724c3",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_418e8ffd-d525-4a87-9904-2e89d9df1c8e_right",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_418e8ffd-d525-4a87-9904-2e89d9df1c8e_left",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_e53d959e-78cc-4e8c-b298-5247c646a58f_sixminutewalktesttest",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_0f0972aa-7b4e-4e8a-881c-5dfa5ba049c9_type",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_87eaa88a-54b6-440f-941d-ff643757dc66",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_945951c5-5409-4389-964c-757b3a769bb3_description",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_f5649cb6-e52a-4bf6-877c-703e851c3bd8_description",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_f5649cb6-e52a-4bf6-877c-703e851c3bd8_pitting",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_a865bd45-04f8-4f32-b396-f78934018040",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_a865bd45-04f8-4f32-b396-f78934018040",
-        "style": "margin-left: 10px;"
-    },
-    {
-        "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "observation_28db794a-c08e-459c-9f85-60199af686cc",
+        "name": "observation_additionalComments",
         "style": "margin-left: 10px;"
     }
 ]
