f566bda3084b734f0f34d42a0058c411942f30df
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-07-24 13:57:25 +0300

    add first level of Redcord Neurac Stability Tests

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/strength/strength.fields.styles.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/strength/strength.fields.styles.ts
@@ -101,5 +101,52 @@ export var StrengthFormStyles: FieldControlStyles[] = [
         "label_style": "white-space: nowrap; min-width: 550px;",
         "name": "strength_1d31270d-f842-416b-a2df-af1849c4da70_hand",
         "style": "margin-left: 10px;"
+    },
+    //
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_71a9aa89-6780-4280-9f31-978d12162bc2_hip",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_71a9aa89-6780-4280-9f31-978d12162bc2_knee",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_71a9aa89-6780-4280-9f31-978d12162bc2_ankle",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_71a9aa89-6780-4280-9f31-978d12162bc2_foot",
+        "style": "margin-left: 10px;"
+    },
+    //
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_8a3c5251-1268-43b7-881a-4f03fbb246d7_upperBodyMyofascialTests",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_8a3c5251-1268-43b7-881a-4f03fbb246d7_lowerBodyMyofascialTests",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_8a3c5251-1268-43b7-881a-4f03fbb246d7_cervicalMovements",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_8a3c5251-1268-43b7-881a-4f03fbb246d7_cervicalMotorControlTests",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_8a3c5251-1268-43b7-881a-4f03fbb246d7_lumbarMotorControlTests",
+        "style": "margin-left: 10px;"
     }
 ]
