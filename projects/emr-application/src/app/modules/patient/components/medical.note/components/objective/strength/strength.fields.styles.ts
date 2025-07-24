d61d6c3c4435cae7c49d1cbe8a5b726ce3705d89
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-07-24 13:53:31 +0300

    add first level of Selective Tissue Tension Upper

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/strength/strength.fields.styles.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/strength/strength.fields.styles.ts
@@ -65,5 +65,41 @@ export var StrengthFormStyles: FieldControlStyles[] = [
         "label_style": "white-space: nowrap; min-width: 500px;",
         "name": "strength_a2ea714a-7521-429e-ba0a-46b51da47dd8_lowerExtremity",
         "style": "margin-left: 10px;"
+    },
+    //
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_1d31270d-f842-416b-a2df-af1849c4da70_cervical",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_1d31270d-f842-416b-a2df-af1849c4da70_trunk",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_1d31270d-f842-416b-a2df-af1849c4da70_backRibs",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_1d31270d-f842-416b-a2df-af1849c4da70_shoulder",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_1d31270d-f842-416b-a2df-af1849c4da70_elbow",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_1d31270d-f842-416b-a2df-af1849c4da70_wrist",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 550px;",
+        "name": "strength_1d31270d-f842-416b-a2df-af1849c4da70_hand",
+        "style": "margin-left: 10px;"
     }
 ]
