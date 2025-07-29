584adac37fe385d878f1298630fd4050def90765
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-07-29 17:22:35 +0300

    add Thomas Test deps

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/special.tests/st.fields.styles.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/special.tests/st.fields.styles.ts
@@ -233,6 +233,11 @@ export var SpecialTestStyles: FieldControlStyles[] = [
         "label_style": "white-space: nowrap; min-width: 500px;",
         "name": "special_tests_65d4e098-37d5-4ba8-99e8-010dc2cc60bc_",
         "style": "margin-left: 10px;width:800px"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "special_tests_43a7fb9d-6c04-4b37-8c6f-a8f1f47f8752_specialteststhomastesttoprightleftcontrols",
+        "style": "margin-left: 10px;"
     }
 ]
 
