6d1c473027d21d74eaccdf6fca0e0950b13b7530
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-08-06 13:28:57 +0300

    finish Palpation section

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/palpation/palpation.fields.styles.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/palpation/palpation.fields.styles.ts
@@ -18,7 +18,262 @@ export var PalpationStyles: FieldControlStyles[] = [
     },
     {
         "label_style": "white-space: nowrap; min-width: 500px;",
-        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponent",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentone",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentthree",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentsacralmultifidii",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentasispsisischial",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentrotatorcuff",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentpiriformis",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentposteriorcuff",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentgluteus",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentecrlbiceps",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponenttriceps",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentbicepsandother",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentextensor",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentflexor",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentpronatorecrb",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponenttubercle",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentthenar",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentscaphoid",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentsnuffbox",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponenthypothenareminence",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentiliopsoas",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponenttfl",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentgreatertrochantericbursa",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponenthamstrings",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentquadriceps",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentrectusfemoris",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentitband",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponenttibialtuberosity",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentlateraljointline",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentmedialjointline",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentpesanserine",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentlcl",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentmcl",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentanteriortibialis",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentposteriortibialis",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentperoneals",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentgastrocsoleusjunction",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentachillestendon",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentcalcaneus",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentretrocalcanealbursa",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentdeltoidligaments",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentanteriortalofibularligament",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentcalcaneofibularligament",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentposteriortalofibularligament",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentplantarfascia",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentmortoninterdigit",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentfmetatarsalhead",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentsmetatarsalhead",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentthmetatarsalhead",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentfourthmetatarsalhead",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentfifthmetatarsalhead",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_7f7a48a6-f93d-4494-81cd-91a38daacb46_palpationcustomcomponentsesamoids",
+        "style": "margin-left: 10px;"
+    },
+    {
+        "label_style": "white-space: nowrap; min-width: 500px;",
+        "name": "palpation_9bd02027-67a1-49d9-8029-c2657b35594a_palpationadditionalcomments",
         "style": "margin-left: 10px;"
     }
 ]
