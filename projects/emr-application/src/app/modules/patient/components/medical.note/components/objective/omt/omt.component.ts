c711e8fa2da235851e32c46af03a3e6e8429bbbb
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-08-17 15:53:02 +0300

    remove un-used input in omt component

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/omt/omt.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/omt/omt.component.ts
@@ -14,7 +14,6 @@ export class OmtComponent implements OnInit {
   omtForm: FormGroup;
   @Input() fields: any
   @Output() formReady = new EventEmitter<FormGroup>();
-  @Input() data: any
   styles: FieldControlStyles[] = OMTFormStyles;
   controls: OMTTestControl[] = []
   constructor(private fb: FormBuilder
