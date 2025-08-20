4adef9f928c7a6fa39eaf1df8a86077e519d334e
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-08-20 14:47:06 +0300

    fill daily note plan of care data

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/plan/daily/daily-plan.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/plan/daily/daily-plan.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
@@ -8,6 +8,8 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class DailyPlanComponent implements OnInit {
   dailyPlanForm: FormGroup;
+  @Output() formReady = new EventEmitter<FormGroup>();
+  @Input() planData: any
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
@@ -15,6 +17,13 @@ export class DailyPlanComponent implements OnInit {
       instructions: ['DN1'],
       instructionTxt: [''],
     })
+    if (this.planData && this.planData['instructions']) {
+      this.dailyPlanForm.get('instructions').setValue(this.planData['instructions'])
+    }
+    if (this.planData && this.planData['instructionTxt']) {
+      this.dailyPlanForm.get('instructionTxt').setValue(this.planData['instructionTxt'])
+    }
+    this.formReady.emit(this.dailyPlanForm);
   }
 
 }
