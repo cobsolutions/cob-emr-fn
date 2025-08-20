763c6a1462bea2224257c831bea8b45c9221506d
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-08-20 14:36:09 +0300

    design plan of daily note

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/plan/daily/daily-plan.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/plan/daily/daily-plan.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-daily-plan',
+  selector: 'daily-plan',
   templateUrl: './daily-plan.component.html',
   styleUrls: ['./daily-plan.component.css']
 })
 export class DailyPlanComponent implements OnInit {
-
-  constructor() { }
+  dailyPlanForm: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.dailyPlanForm = this.fb.group({
+      instructions: ['DN1'],
+      instructionTxt: [''],
+    })
   }
 
 }
