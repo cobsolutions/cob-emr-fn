a7e63ca2e10da4253541948d35d7b36bbde25349
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-08-25 00:06:35 +0300

    create discharge form and populate it

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/plan/discharge/discharge-plan.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/plan/discharge/discharge-plan.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatStepper } from '@angular/material/stepper';
 
 @Component({
-  selector: 'app-discharge-plan',
+  selector: 'discharge-plan',
   templateUrl: './discharge-plan.component.html',
   styleUrls: ['./discharge-plan.component.css']
 })
 export class DischargePlanComponent implements OnInit {
-
-  constructor() { }
+  dischargePlanForm: FormGroup;
+  @Output() formReady = new EventEmitter<FormGroup>();
+  @Input() planData: any
+  @Input() noteId: number
+  @Input() noteType: string
+  @Input() stepper!: MatStepper
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.dischargePlanForm = this.fb.group({
+      reason: ['D01'],
+      discharge: ['DN1'],
+      physicianSignature: [false]
+    })
   }
 
 }
