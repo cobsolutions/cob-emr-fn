eaa2f750837f14b250fdb575b169262599e505b7
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-09-08 00:39:07 +0300

    add modal to view summary

--- projects/emr-application/src/app/modules/patient/components/chart/patient.case/patient-chart-case.component.ts
+++ projects/emr-application/src/app/modules/patient/components/chart/patient.case/patient-chart-case.component.ts
@@ -43,6 +43,7 @@ export class PatientChartCaseComponent extends ListTemplate implements OnInit {
   errorMessage: string;
   showTest: boolean = false;
   componentRole: string[] = [Role.INITIALIZE_MEDICAL_NOTE_ROLE];
+  viewPDFVisibility:boolean = false;
   constructor(
     private patientRecordService: PatientRecordService,
     private medialNoteService: MedialNoteService,
@@ -58,6 +59,9 @@ export class PatientChartCaseComponent extends ListTemplate implements OnInit {
   }
   toggleOMTVisibility() {
     this.showTest = !this.showTest;
+  }
+  toggleviewPDFVisibility() {
+    this.viewPDFVisibility = !this.viewPDFVisibility;
   }
   show() {
     this.showTest = true;
@@ -81,7 +85,7 @@ export class PatientChartCaseComponent extends ListTemplate implements OnInit {
     const patientRecordRequest: PatientRecordRequest = {
       patientId: this.patientId,
       caseId: this.case.id,
-      loggedIn : this.loggedInService.getLoggedUser().uuid
+      loggedIn: this.loggedInService.getLoggedUser().uuid
     }
     this.patientRecords$ = this.patientRecordService.find(this.apiParams$, patientRecordRequest).pipe(
       retry({
@@ -165,12 +169,17 @@ export class PatientChartCaseComponent extends ListTemplate implements OnInit {
     })
   }
   executeRecordLineAction(val: string, entityId: number, status?: string) {
+    console.log(val)
     if (val === 'View Reason')
       this.getAppointment(entityId)
     if (val === 'Remove')
       this.removeMedicalNote(entityId);
     if (val === 'Complete') {
       this.completeMedicalNote(entityId, status)
+    }
+    if (val === 'View Pdf') {
+      this.viewPDFVisibility = true;
+      console.log('entityId ' + entityId + ' status ' + status)
     }
   }
   handleBackAction() {
