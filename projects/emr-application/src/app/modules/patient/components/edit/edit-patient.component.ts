1b893f92f5c43d91236263944e38dc2e8d54e106
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-09-16 16:42:50 +0300

    add design for edit patient component

--- projects/emr-application/src/app/modules/patient/components/edit/edit-patient.component.ts
+++ projects/emr-application/src/app/modules/patient/components/edit/edit-patient.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { filter, switchMap, tap } from 'rxjs';
+import { LoggedInService } from '../../../security/service/loggedIn/logged-in.service';
+import { PateintResponse } from '../../models/response/patient.response';
+import { PatientFinderService } from '../../services/patient/patient-finder.service';
 
 @Component({
   selector: 'app-edit-patient',
@@ -7,12 +11,57 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./edit-patient.component.css']
 })
 export class EditPatientComponent implements OnInit {
-  patientId:number;
-  constructor(private route: ActivatedRoute) { }
+deleteAddress(_t22: number) {
+throw new Error('Method not implemented.');
+}
+addAddress() {
+throw new Error('Method not implemented.');
+}
+deleteContact(_t32: number) {
+throw new Error('Method not implemented.');
+}
+addContact() {
+throw new Error('Method not implemented.');
+}
+deleteIdentification(_t42: number) {
+throw new Error('Method not implemented.');
+}
+addIdentification() {
+throw new Error('Method not implemented.');
+}
+deleteInsurance(_t52: number) {
+throw new Error('Method not implemented.');
+}
+addInsurance() {
+throw new Error('Method not implemented.');
+}
+deleteCase(_t62: number) {
+throw new Error('Method not implemented.');
+}
+addCase() {
+throw new Error('Method not implemented.');
+}
+deleteClinic(_t72: number) {
+throw new Error('Method not implemented.');
+}
+addClinic() {
+throw new Error('Method not implemented.');
+}
+  patientId: number;
+  patient: any
+  constructor(private route: ActivatedRoute, private patientFinderService: PatientFinderService, private loggedInService: LoggedInService) { }
 
   ngOnInit(): void {
     this.patientId = Number(this.route.snapshot.paramMap.get('patientId'))
     console.log(this.patientId)
+    this.loggedInService.selectedClinic$
+      .pipe(
+        filter(clinicId => clinicId != null),
+        switchMap((clinicId) => this.patientFinderService.getPatient(this.patientId, clinicId)))
+      .subscribe((response: PateintResponse) => {
+        this.patient = response.records
+      }, error => {
+      })
   }
 
 }
