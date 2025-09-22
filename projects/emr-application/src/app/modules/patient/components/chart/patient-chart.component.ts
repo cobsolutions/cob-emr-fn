b10bf87be8b7d92c778b0b2ce79d58f5847eadca
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-09-22 21:23:43 +0300

    label patient main info and add gender info

--- projects/emr-application/src/app/modules/patient/components/chart/patient-chart.component.ts
+++ projects/emr-application/src/app/modules/patient/components/chart/patient-chart.component.ts
@@ -36,6 +36,7 @@ export class PatientChartComponent implements OnInit {
   caseId: number = 0;
   clinicId: number;
   selectedIndex = 0;  // Default to first tab
+  showFullAddress = false;
   constructor(private route: ActivatedRoute
     , private patientFinderService: PatientFinderService
     , private pateintCaseService: PateintCaseService
