94158c8b2851849eb87dbe8c475fbd4601c58383
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-10-23 01:30:11 +0300

    enhance TopRightLeftSelectsComponent with custom and design

--- projects/emr-application/src/app/modules/patient/components/medical.note/custom.input/strength/top.right.left.selects/top-right-left-selects.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/custom.input/strength/top.right.left.selects/top-right-left-selects.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'top-right-left-selects',
@@ -19,6 +20,7 @@ export class TopRightLeftSelectsComponent implements OnInit {
   @Input() leftLabel: string = 'Left';
   @Input() showLabel: boolean = true;
   @Input() hasComment: boolean = false;
+  private subscriptions: Subscription[] = [];
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({});
   }
@@ -32,9 +34,7 @@ export class TopRightLeftSelectsComponent implements OnInit {
       this.topControls.forEach(control => {
         const key = `top_${this.toCamelCase(control.label)}_${this.parentFieldName}`;
         this.form.addControl(key, new FormControl(control.value[0].val));
-        this.form.get(key)?.valueChanges.subscribe(() => {
-          this.parentForm.get(this.parentFieldName)?.setValue(this.form.getRawValue());
-        });
+        this.handleValueChanges(key);
       });
 
     // Right controls
@@ -42,9 +42,7 @@ export class TopRightLeftSelectsComponent implements OnInit {
       this.controlR.forEach(control => {
         const key = `right_${this.toCamelCase(control.label)}_${this.parentFieldName}`;
         this.form.addControl(key, new FormControl(control?.value[0].val));
-        this.form.get(key)?.valueChanges.subscribe(() => {
-          this.parentForm.get(this.parentFieldName)?.setValue(this.form.getRawValue());
-        });
+        this.handleValueChanges(key);
       });
 
     // Left controls
@@ -52,9 +50,7 @@ export class TopRightLeftSelectsComponent implements OnInit {
       this.controlL.forEach(control => {
         const key = `left_${this.toCamelCase(control.label)}_${this.parentFieldName}`;
         this.form.addControl(key, new FormControl(control?.value[0].val));
-        this.form.get(key)?.valueChanges.subscribe(() => {
-          this.parentForm.get(this.parentFieldName)?.setValue(this.form.getRawValue());
-        });
+        this.handleValueChanges(key);
       });
     if (this.hasComment) {
       const key = `comment_${this.parentFieldName}`;
@@ -72,5 +68,31 @@ export class TopRightLeftSelectsComponent implements OnInit {
   }
   getCommentName(): string {
     return `comment_${this.parentFieldName}`;
+  }
+  private handleValueChanges(controlName: string) {
+    const sub = this.form.get(controlName).valueChanges.subscribe(value => {
+      const customControlName = `${controlName}_custom`;
+  
+      if (value === 'Custom' && !this.form.get(customControlName)) {
+        // ✅ Add the input FormControl
+        this.form.addControl(customControlName, new FormControl(''));
+  
+        // ✅ Subscribe to its changes as well
+        const customSub = this.form.get(customControlName).valueChanges.subscribe(customValue => {
+          this.parentForm.get(this.parentFieldName)?.setValue(this.form.getRawValue());
+        });
+        this.subscriptions.push(customSub);
+      }
+  
+      // ✅ If changed to non-custom value, remove control
+      if (value !== 'Custom' && this.form.get(customControlName)) {
+        this.form.removeControl(customControlName);
+      }
+  
+      // ✅ Always sync parent form
+      this.parentForm.get(this.parentFieldName)?.setValue(this.form.getRawValue());
+    });
+  
+    this.subscriptions.push(sub);
   }
 }
