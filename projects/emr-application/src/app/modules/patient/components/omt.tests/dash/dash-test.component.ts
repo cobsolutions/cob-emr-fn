fc8219db341f735a2ca73b26d3ba76983c1d21d6
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-10-23 19:41:00 +0300

    save and render values of omt dash test

--- projects/emr-application/src/app/modules/patient/components/omt.tests/dash/dash-test.component.ts
+++ projects/emr-application/src/app/modules/patient/components/omt.tests/dash/dash-test.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { OMTTestValues } from '../../../models/medical.note/omt.test/omt.test.values';
+import { MedialNoteService } from '../../../services/medical.note/medial-note.service';
 import { OmtTestService } from '../../../services/test/omt-test.service';
 import { dashValidator } from '../validator/not.selected';
 
@@ -11,6 +13,9 @@ import { dashValidator } from '../validator/not.selected';
 export class DashTestComponent implements OnInit {
   dashForm: FormGroup;
   showInstructions = false;
+  private testName: string = 'dash';
+  medicalNoteId: number;
+  id: number
   @Output() getResult = new EventEmitter<any>()
   // Question labels for the form
   questions = [
@@ -99,10 +104,23 @@ export class DashTestComponent implements OnInit {
     { value: '5', text: 'Strongly Agree' }
   ];
 
-  constructor(private fb: FormBuilder, private omtTestService: OmtTestService) {
+  constructor(private fb: FormBuilder
+    , private omtTestService: OmtTestService
+    , private medicalNotService: MedialNoteService) {
     this.dashForm = this.createForm();
   }
   ngOnInit(): void {
+    this.medicalNotService.medicalNoteID$.subscribe(id => {
+      this.medicalNoteId = id
+      this.omtTestService.findValues(this.medicalNoteId, this.testName).subscribe((data: any) => {
+        this.id = data?.id
+        setTimeout(() => {
+          this.dashForm.patchValue(data.values);
+        }, 10);
+
+      })
+      console.log('medial Note ID ' + id)
+    })
   }
   createForm(): FormGroup {
     const formGroup: any = {};
@@ -165,6 +183,14 @@ export class DashTestComponent implements OnInit {
       }
     }
     this.omtTestService.dashTest(result).subscribe(val => {
+      var omtTestValues: OMTTestValues = {
+        id: this.id,
+        medicalNoteId: this.medicalNoteId,
+        testName: this.testName,
+        values: this.dashForm.getRawValue()
+      };
+      this.omtTestService.saveValues(omtTestValues).subscribe(val => {
+      })
       this.getResult.emit(val)
     })
   }
