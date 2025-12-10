35366fbf846e4f1f5d80a2f8790ad9c019f9abb1
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-12-10 18:21:26 +0200

    impl Initial Exam Note Service

---
+++ projects/emr-application/src/app/modules/patient/services/medical.note/initial.exam/initial-exam-note.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'projects/emr-application/src/environments/environment';
+import { CreateNodeRequest } from '../../../models/medical.note/create.note.request';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class InitialExamNoteService {
+  private baseUrl = environment.baseURL + 'medical/note/initial-exam'
+  constructor(private httpClient: HttpClient) { }
+  create(request: CreateNodeRequest) {
+    const headers = { 'content-type': 'application/json' }
+    var url = this.baseUrl 
+    return this.httpClient.post(`${url}`, JSON.stringify(request), { 'headers': headers })
+  }
+}
