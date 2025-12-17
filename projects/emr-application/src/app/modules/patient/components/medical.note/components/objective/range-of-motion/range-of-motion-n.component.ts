73fbfe6e0c219e330a1c5b86db8166ad9e0f087d
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-12-17 16:44:44 +0200

    add change value for hip_prom

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/range-of-motion/range-of-motion-n.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/range-of-motion/range-of-motion-n.component.ts
@@ -555,33 +555,34 @@ export class RangeOfMotionNComponent implements OnInit {
       hip_external_rotation_right: ['not_tested'],
       hip_external_rotation_left: ['not_tested'],
       hip_arrom_comments: [''],
-      
+
       hip_prom: ['no'],
+      hip_prom_apply_to_all: [''],
       hip_prom_flexion_right: ['not_tested'],
       hip_prom_flexion_right_endfeel: ['not_tested'],
       hip_prom_flexion_left: ['not_tested'],
       hip_prom_flexion_left_endfeel: ['not_tested'],
-      
+
       hip_prom_extension_right: ['not_tested'],
       hip_prom_extension_right_endfeel: ['not_tested'],
       hip_prom_extension_left: ['not_tested'],
       hip_prom_extension_left_endfeel: ['not_tested'],
-      
+
       hip_prom_abduction_right: ['not_tested'],
       hip_prom_abduction_right_endfeel: ['not_tested'],
       hip_prom_abduction_left: ['not_tested'],
       hip_prom_abduction_left_endfeel: ['not_tested'],
-      
+
       hip_prom_adduction_right: ['not_tested'],
       hip_prom_adduction_right_endfeel: ['not_tested'],
       hip_prom_adduction_left: ['not_tested'],
       hip_prom_adduction_left_endfeel: ['not_tested'],
-      
+
       hip_prom_internal_rotation_right: ['not_tested'],
       hip_prom_internal_rotation_right_endfeel: ['not_tested'],
       hip_prom_internal_rotation_left: ['not_tested'],
       hip_prom_internal_rotation_left_endfeel: ['not_tested'],
-      
+
       hip_prom_external_rotation_right: ['not_tested'],
       hip_prom_external_rotation_right_endfeel: ['not_tested'],
       hip_prom_external_rotation_left: ['not_tested'],
@@ -1457,6 +1458,40 @@ export class RangeOfMotionNComponent implements OnInit {
 
     this.romForm.get('hip_prom')?.valueChanges.subscribe(value => {
       this.showHipPromFields = value === 'yes';
+      if (value === 'no') {
+        this.romForm.patchValue({
+          hip_prom_apply_to_all: '',
+          hip_prom_flexion_right: 'not_tested',
+          hip_prom_flexion_right_endfeel: 'not_tested',
+          hip_prom_flexion_left: 'not_tested',
+          hip_prom_flexion_left_endfeel: 'not_tested',
+
+          hip_prom_extension_right: 'not_tested',
+          hip_prom_extension_right_endfeel: 'not_tested',
+          hip_prom_extension_left: 'not_tested',
+          hip_prom_extension_left_endfeel: 'not_tested',
+
+          hip_prom_abduction_right: 'not_tested',
+          hip_prom_abduction_right_endfeel: 'not_tested',
+          hip_prom_abduction_left: 'not_tested',
+          hip_prom_abduction_left_endfeel: 'not_tested',
+
+          hip_prom_adduction_right: 'not_tested',
+          hip_prom_adduction_right_endfeel: 'not_tested',
+          hip_prom_adduction_left: 'not_tested',
+          hip_prom_adduction_left_endfeel: 'not_tested',
+
+          hip_prom_internal_rotation_right: 'not_tested',
+          hip_prom_internal_rotation_right_endfeel: 'not_tested',
+          hip_prom_internal_rotation_left: 'not_tested',
+          hip_prom_internal_rotation_left_endfeel: 'not_tested',
+
+          hip_prom_external_rotation_right: 'not_tested',
+          hip_prom_external_rotation_right_endfeel: 'not_tested',
+          hip_prom_external_rotation_left: 'not_tested',
+          hip_prom_external_rotation_left_endfeel: 'not_tested'
+        })
+      }
     });
 
     this.romForm.get('knee_arrom')?.valueChanges.subscribe(value => {
