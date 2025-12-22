e303262f1949296e0790b754e2296428894adb1f
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-12-22 23:25:05 +0200

    add skeleton of materialHandling component

---
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/common/material-handling/material-handling.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-material-handling',
+  templateUrl: './material-handling.component.html',
+  styleUrls: ['./material-handling.component.css']
+})
+export class MaterialHandlingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
