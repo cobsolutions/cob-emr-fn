cd93ec2b9a5912bd56789e7d4343290b317a4850
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-12-22 23:26:07 +0200

    add skeleton of nonMaterialHandling

---
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/common/non-material-handling/non-material-handling.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-non-material-handling',
+  templateUrl: './non-material-handling.component.html',
+  styleUrls: ['./non-material-handling.component.css']
+})
+export class NonMaterialHandlingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
