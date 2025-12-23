d4ea8cc275f3665318029a5bb64464046002f0c4
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-12-23 14:02:41 +0200

    add musle config

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/palpationN/config/index.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/palpationN/config/index.ts
@@ -1,7 +1,27 @@
 import { MuscleAssessmentConfig } from "./muscle-assessment-config";
 import { TemperaturePalpation } from "./temperature-palpation-config";
 
-export class PalpationConfig{
+export class PalpationConfig {
     static readonly temperaturePalpation = TemperaturePalpation.temperaturePalpation;
     static readonly rightTemporalis = MuscleAssessmentConfig.rightTemporalis;
+    static readonly leftTemporalis = MuscleAssessmentConfig.leftTemporalis;
+    static readonly rightMasseter = MuscleAssessmentConfig.rightMasseter;
+    static readonly leftMasseter = MuscleAssessmentConfig.leftMasseter;
+    static readonly rightScm = MuscleAssessmentConfig.rightScm;
+    static readonly leftScm = MuscleAssessmentConfig.leftScm;
+    static readonly rightScalenes = MuscleAssessmentConfig.rightScalenes;
+    static readonly leftScalenes = MuscleAssessmentConfig.leftScalenes;
+    static readonly rightSuboccipitalMyofascia = MuscleAssessmentConfig.rightSuboccipitalMyofascia;
+    static readonly leftSuboccipitalMyofascia = MuscleAssessmentConfig.leftSuboccipitalMyofascia;
+    static readonly rightLevatorScapula = MuscleAssessmentConfig.rightLevatorScapula;
+    static readonly leftLevatorScapula = MuscleAssessmentConfig.leftLevatorScapula;
+    static readonly rightUpperTrapezius = MuscleAssessmentConfig.rightUpperTrapezius;
+    static readonly leftUpperTrapezius = MuscleAssessmentConfig.leftUpperTrapezius;
+    static readonly rightMiddleTrapezius = MuscleAssessmentConfig.rightMiddleTrapezius;
+    static readonly leftMiddleTrapezius = MuscleAssessmentConfig.leftMiddleTrapezius;
+    static readonly rightLowerTrapezius = MuscleAssessmentConfig.rightLowerTrapezius;
+    static readonly leftLowerTrapezius = MuscleAssessmentConfig.leftLowerTrapezius;
+    static readonly rightQuadratusLumborum = MuscleAssessmentConfig.rightQuadratusLumborum;
+    static readonly leftQuadratusLumborum = MuscleAssessmentConfig.leftQuadratusLumborum;
+
 }
