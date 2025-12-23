0c2b140ef4a83234b0a444e36adfcf4bcbf082b1
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-12-23 13:11:51 +0200

    TemperaturePalpation done

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/palpationN/config/index.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/palpationN/config/index.ts
@@ -1,3 +1,5 @@
+import { TemperaturePalpation } from "./temperature-palpation-config";
+
 export class PalpationConfig{
-    
+    static readonly temperaturePalpation = TemperaturePalpation.temperaturePalpation;
 }
--- projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/palpationN/palpation-n.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/objective/palpationN/palpation-n.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { PalpationConfig } from './config';
+
 
 @Component({
   selector: 'palpation-n',
@@ -9,8 +11,12 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class PalpationNComponent implements OnInit {
   palpationTestForm: FormGroup;
   @Output() formReady = new EventEmitter<FormGroup>();
+  readonly PalpationConfig = PalpationConfig
+
   showPalpationFields: boolean = false;
+  showPalpationTemperaturPpalpationFields: boolean = false;
   showAdditionalCommentsFields: boolean = false;
+  
   constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
@@ -19,7 +25,9 @@ export class PalpationNComponent implements OnInit {
   }
   initForm() {
     this.palpationTestForm = this.fb.group({
+
       palpation: ['no'],
+      temperature_palpation: ['no'],
 
       additional_comments: ['no'],
       additional_comments_text: ['']
@@ -29,7 +37,10 @@ export class PalpationNComponent implements OnInit {
     this.palpationTestForm.get('palpation')?.valueChanges.subscribe(value => {
       this.showPalpationFields = value === 'yes';
     });
-    
+    this.palpationTestForm.get('temperature_palpation')?.valueChanges.subscribe(value => {
+      this.showPalpationTemperaturPpalpationFields = value === 'yes';
+    });
+
     this.palpationTestForm.get('additional_comments')?.valueChanges.subscribe(value => {
       this.showAdditionalCommentsFields = value === 'yes';
     });
