447f726523cefae3a2712c6e13d32385ec4ad1d2
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-12-24 21:19:16 +0200

    add comments to ListCheckboxWithChildComponent

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/subjective/common/list-checkbox-with-child/list-checkbox-with-child.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/subjective/common/list-checkbox-with-child/list-checkbox-with-child.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-list-checkbox-with-child',
@@ -6,7 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-checkbox-with-child.component.css']
 })
 export class ListCheckboxWithChildComponent implements OnInit {
-
+  @Input() formGroup!: FormGroup;
+  @Input() title:string
+  /*
+    TODO
+    Vertical checkboxes with label.
+    each checkbox has its child may be input text or textArea.
+    all checkboxes and its child add to formGroup with prefix with title
+    prefix title will be lowercase underscore between words if found
+    Style of these checkboxes is lie what u did in vertical checkboxes.
+  */
   constructor() { }
 
   ngOnInit(): void {
