c735378a9adadb170a302b5d339cbc7533b6e327
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-12-26 21:22:36 +0200

    create skeleton of directTimedCodes

---
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/billing/componentsN/direct-timed-codes/direct-timed-codes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-direct-timed-codes',
+  templateUrl: './direct-timed-codes.component.html',
+  styleUrls: ['./direct-timed-codes.component.css']
+})
+export class DirectTimedCodesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
