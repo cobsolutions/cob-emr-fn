a3601b011a89590941bb231d48c7a0887cbd4768
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-12-29 04:55:52 +0200

    add NeuroVascular model and impl mapper

--- projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/initial-examination.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/initial.examination/initial-examination.component.ts
@@ -120,11 +120,11 @@ export class InitialExaminationComponent implements OnInit {
     if (action === 'back')
       this.backtoPatientRecordActions()
     if (action === 'draft') {
-      // const formValues = this.getAllFormValues(this.initialExaminationForm);
-      // console.log('Form Values:', formValues.objective.palpation);
-      // console.log('Mapped Subjective:', this.subjectiveMapper.toModel(formValues.subjective));
-      var medicalNoteRequest: MedicalNoteRequest = this.buildMedicalNoteModel();
-      console.log(medicalNoteRequest)
+      const formValues = this.getAllFormValues(this.initialExaminationForm);
+      console.log('Form Values:', formValues.objective.neuroVascular);
+      console.log('Mapped Subjective:', this.subjectiveMapper.toModel(formValues.subjective));
+      // var medicalNoteRequest: MedicalNoteRequest = this.buildMedicalNoteModel();
+      // console.log(medicalNoteRequest)
     }
     // this.draft();
   }
