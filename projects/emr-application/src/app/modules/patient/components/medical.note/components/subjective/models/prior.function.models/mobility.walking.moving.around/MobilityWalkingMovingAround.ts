dc150e62ab788e97424fbef714589f7ceb27d260
madel <mohammed.adel.abo.foutouh@gmail.com>
2025-12-30 13:27:21 +0200

    add flag to other parents in care self models

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/subjective/models/prior.function.models/mobility.walking.moving.around/MobilityWalkingMovingAround.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/subjective/models/prior.function.models/mobility.walking.moving.around/MobilityWalkingMovingAround.ts
@@ -6,15 +6,24 @@ import { NegotiateObstacles } from "./NegotiateObstacles";
 
 export interface MobilityWalkingMovingAround {
   //"prior-level-function_mobility-walking-moving-around_mobility_iadls": false,
+  iADLsFlag:boolean
   iADLs: IADLs;
+
   // "prior-level-function_mobility-walking-moving-around_assistive_device": false,
   useOfAnAssistiveDevice: boolean;
   //"prior-level-function_mobility-walking-moving-around_walking": false,
+  walkingFlag:boolean;
   walking: Walking;
+
   // "prior-level-function_mobility-walking-moving-around_moving_around": false,
+  movingAroundFlag: boolean;
   movingAround: MovingAround;
+
   "prior-level-function_mobility-walking-moving-around_different_locations": false,
+  movingAroundInDifferentLocationsFlag:boolean
   movingAroundInDifferentLocations: MovingAroundInDifferentLocations;
+
   // "prior-level-function_mobility-walking-moving-around_negotiate_obstacles": false,
+  negotiateObstaclesFlag:boolean
   negotiateObstacles: NegotiateObstacles;
 }
--- projects/emr-application/src/app/modules/patient/components/medical.note/components/subjective/models/prior.function.models/mobility.walking.moving.around/MovingAroundInDifferentLocations.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/subjective/models/prior.function.models/mobility.walking.moving.around/MovingAroundInDifferentLocations.ts
@@ -5,13 +5,21 @@ import { MovingAroundUsingTransportation } from "./MovingAroundUsingTransportati
 
 export interface MovingAroundInDifferentLocations {
   //"prior-level-function_mobility-walking-moving-around_different_locations_between_rooms": false,  
+  walkingBetweenRoomsFlag:boolean;
   walkingBetweenRooms: WalkingBetweenRooms;
+
   // "prior-level-function_mobility-walking-moving-around_different_locations_down_street": false,
+  walkingDownTheStreetFlag:boolean
   walkingDownTheStreet: WalkingDownTheStreet;
+
   // "prior-level-function_mobility-walking-moving-around_different_locations_within_building": false,
   walkingWithinABuilding: boolean;
+
   // "prior-level-function_mobility-walking-moving-around_different_locations_using_equipment": false,
+  movingAroundUsingEquipmentFlag:boolean
   movingAroundUsingEquipment: MovingAroundUsingEquipment;
+
   // "prior-level-function_mobility-walking-moving-around_different_locations_using_transportation": false,
+  movingAroundUsingTransportationFlag:boolean
   movingAroundUsingTransportation: MovingAroundUsingTransportation;
 }
