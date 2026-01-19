85250d7413e873d4088ad691b06ed0e8f9cb718f
madel <mohammed.adel.abo.foutouh@gmail.com>
2026-01-19 12:52:11 +0200

    call finalzie medical note

--- projects/emr-application/src/app/modules/patient/components/medical.note/components/billing/billing-n.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/components/billing/billing-n.component.ts
@@ -1,8 +1,15 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatStepper } from '@angular/material/stepper';
+import * as moment from 'moment';
+import { MedicalNoteRequest } from '../../../../models/medical.note/medical.note.request';
 import { MedicalNoteType } from '../../../../models/medical.note/medical.note.type';
 import { ToastrService } from 'ngx-toastr';
+import { SubjectiveMapperService } from '../subjective/services/subjective-mapper.service';
+import { ObjectiveMapperService } from '../objective/service/objective-mapper.service';
+import { AssessmentMapperService } from '../assessment/service/assessment-mapper.service';
+import { PlanOfCareMapperService } from '../plan/service/plan-of-care-mapper.service';
+import { BillingMapperService } from './service/billing-mapper.service';
 
 @Component({
   selector: 'billing-n',
@@ -12,16 +19,27 @@ import { ToastrService } from 'ngx-toastr';
 export class BillingNComponent implements OnInit {
   @Input() parentForm: FormGroup
   @Output() formReady = new EventEmitter<FormGroup>();
+  @Output() backToRecords = new EventEmitter<void>();
   @Input() stepper!: MatStepper
   @Input() noteTypeId: string
   @Input() caseId: string
   @Input() medicalNoteId: number
+  @Input() noteId: string
   @Input() billingData: any;
   BillingForm: FormGroup;
   finalizeNoteVisibility: boolean = false;
   forwardVisibility: boolean = false;
   noteType: MedicalNoteType = MedicalNoteType.Initial_Examination;
-  constructor(private fb: FormBuilder, private toastr: ToastrService) { }
+  medicalNoteRequest: MedicalNoteRequest;
+  constructor(
+    private fb: FormBuilder,
+    private toastr: ToastrService,
+    private subjectiveMapper: SubjectiveMapperService,
+    private objectiveMapperService: ObjectiveMapperService,
+    private assessmentMapper: AssessmentMapperService,
+    private planOfCareMapper: PlanOfCareMapperService,
+    private billingMapperService: BillingMapperService
+  ) { }
 
   ngOnInit(): void {
     this.initForm();
@@ -67,6 +85,7 @@ export class BillingNComponent implements OnInit {
   }
 
   showFinalizeNotePopup() {
+    this.medicalNoteRequest = this.buildMedicalNoteModel();
     this.finalizeNoteVisibility = true;
   }
 
@@ -77,6 +96,93 @@ export class BillingNComponent implements OnInit {
     if (event === 'yes') {
       this.finalizeNoteVisibility = false;
       this.toastr.success('Medical note has been finalized');
+      this.backToRecords.emit();
     }
+  }
+
+  private buildMedicalNoteModel(): MedicalNoteRequest {
+    const subjectiveGroup = this.parentForm.get('subjective') as FormGroup;
+    const objectiveGroup = this.parentForm.get('objective') as FormGroup;
+    const assessmentGroup = this.parentForm.get('assessment') as FormGroup;
+    const planOfCareGroup = this.parentForm.get('planOfCare') as FormGroup;
+    const billingGroup = this.parentForm.get('billing') as FormGroup;
+
+    var medicalNoteRequest: MedicalNoteRequest = {
+      patientCaseId: this.caseId,
+      id: this.medicalNoteId,
+      subjective: (subjectiveGroup && !this.isFormGroupEmpty(subjectiveGroup))
+        ? this.subjectiveMapper.toModel(subjectiveGroup)
+        : null,
+      objective: (objectiveGroup && !this.isFormGroupEmpty(objectiveGroup))
+        ? this.objectiveMapperService.toModel(objectiveGroup)
+        : null,
+      assessment: (assessmentGroup && !this.isFormGroupEmpty(assessmentGroup))
+        ? this.assessmentMapper.toModel(assessmentGroup)
+        : null,
+      planOfCare: (planOfCareGroup && !this.isFormGroupEmpty(planOfCareGroup))
+        ? this.planOfCareMapper.toModel(planOfCareGroup)
+        : null,
+      billing: (billingGroup && !this.isFormGroupEmpty(billingGroup))
+        ? this.billingMapperService.toModel(billingGroup)
+        : null,
+    }
+
+    if (medicalNoteRequest.subjective?.basic?.dateOfInitialExamination) {
+      medicalNoteRequest.dateOfService = moment(medicalNoteRequest.subjective.basic.dateOfInitialExamination).endOf('day').valueOf();
+    }
+
+    this.normalizeYesNoInObject(medicalNoteRequest);
+    return medicalNoteRequest;
+  }
+
+  private isFormGroupEmpty(formGroup: FormGroup): boolean {
+    if (!formGroup) {
+      return true;
+    }
+    const values = formGroup.value;
+    return Object.values(values).every(val => {
+      if (val && typeof val === 'object') {
+        if (Array.isArray(val)) {
+          return val.length === 0 || val.every(item =>
+            item === null || item === '' || item === undefined
+          );
+        }
+        return Object.values(val).every(nestedVal =>
+          nestedVal === null || nestedVal === '' || nestedVal === undefined
+        );
+      }
+      return val === null || val === '' || val === undefined;
+    });
+  }
+
+  private normalizeYesNoInObject(obj: any): void {
+    if (obj === null || obj === undefined) {
+      return;
+    }
+    if (Array.isArray(obj)) {
+      obj.forEach((item, index) => {
+        if (typeof item === 'string') {
+          obj[index] = this.normalizeValue(item);
+        } else if (typeof item === 'object') {
+          this.normalizeYesNoInObject(item);
+        }
+      });
+    } else if (typeof obj === 'object') {
+      Object.keys(obj).forEach(key => {
+        const value = obj[key];
+        if (typeof value === 'string') {
+          obj[key] = this.normalizeValue(value);
+        } else if (typeof value === 'object') {
+          this.normalizeYesNoInObject(value);
+        }
+      });
+    }
+  }
+
+  private normalizeValue(val: any): any {
+    if (val === 'yes') return true;
+    if (val === 'no') return false;
+    if (val === 'na' || val === 'N/A') return null;
+    return val;
   }
 }
--- projects/emr-application/src/app/modules/patient/components/medical.note/finalize/finalize-medical-note.component.ts
+++ projects/emr-application/src/app/modules/patient/components/medical.note/finalize/finalize-medical-note.component.ts
@@ -3,7 +3,9 @@ import { filter } from 'rxjs';
 
 import { LoggedInService } from '../../../../security/service/loggedIn/logged-in.service';
 import { FinalizeMedicalNoteRequest } from '../../../models/medical.note/finalize.medical.note.request';
+import { MedicalNoteRequest } from '../../../models/medical.note/medical.note.request';
 import { MedicalNoteType } from '../../../models/medical.note/medical.note.type';
+import { InitialExamNoteService } from '../../../services/medical.note/initial.exam/initial-exam-note.service';
 import { MedialNoteService } from '../../../services/medical.note/medial-note.service';
 
 @Component({
@@ -15,11 +17,14 @@ export class FinalizeMedicalNoteComponent implements OnInit {
   @Input() patientCaseId: string;
   @Input() medicalNoteId: number;
   @Input() noteType: MedicalNoteType
+  @Input() medicalNoteRequest: MedicalNoteRequest;
+  @Input() noteId: string;
   @Output() changeVisibility = new EventEmitter<string>()
   finalizeMessage: string = 'The medical note is being finalized. Please be patient.';
   finalizeMessageFlag: boolean = false
   constructor(private medialNoteService: MedialNoteService
-    , private loggedInService: LoggedInService) { }
+    , private loggedInService: LoggedInService
+    , private initialExamNoteService: InitialExamNoteService) { }
   ngOnInit(): void {
   }
   onNo() {
@@ -27,9 +32,17 @@ export class FinalizeMedicalNoteComponent implements OnInit {
     this.medialNoteService.notifyFinalize(false);
   }
   onYes() {
-    this.medialNoteService.notifyFinalize(true);
-    this.changeVisibility.emit('yes')
     this.finalizeMessageFlag = true
+    this.initialExamNoteService.finalize(this.medicalNoteRequest, this.noteId).subscribe({
+      next: (data) => {
+        this.medialNoteService.notifyFinalize(true);
+        this.changeVisibility.emit('yes')
+      },
+      error: (error) => {
+        this.finalizeMessageFlag = false
+        console.error('Finalize failed:', error);
+      }
+    });
   }
 
 }
