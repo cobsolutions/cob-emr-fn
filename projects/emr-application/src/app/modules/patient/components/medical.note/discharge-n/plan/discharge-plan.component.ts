bdfa690fc1eb03c202b6e37da3409552cab1603f
madel <mohammed.adel.abo.foutouh@gmail.com>
2026-01-25 04:29:45 +0200

    implement skeleton of discharge plan

---
+++ projects/emr-application/src/app/modules/patient/components/medical.note/discharge-n/plan/discharge-plan.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-discharge-plan',
+  templateUrl: './discharge-plan.component.html',
+  styleUrls: ['./discharge-plan.component.css']
+})
+export class DischargePlanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
